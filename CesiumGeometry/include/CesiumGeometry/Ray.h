c890158acac58b2c5bacfa174587fc44c080b34e
Kevin Ring <kevin@kotachrome.com>
2020-08-17 15:07:03 +1000

    Doc for Ray class.

--- CesiumGeometry/include/CesiumGeometry/Ray.h
+++ CesiumGeometry/include/CesiumGeometry/Ray.h
@@ -5,13 +5,32 @@
 
 namespace CesiumGeometry {
 
+    /**
+     * @brief A ray that extends infinitely from the provided origin in the provided direction.
+     */
     class CESIUMGEOMETRY_API Ray {
     public:
+        /**
+         * @brief Construct a new ray.
+         * 
+         * @param origin The origin of the ray.
+         * @param direction The direction of the ray.
+         */
         Ray(const glm::dvec3& origin, const glm::dvec3& direction);
 
+        /**
+         * @brief Gets the origin of the ray.
+         */
         const glm::dvec3& getOrigin() const { return this->_origin; }
+
+        /**
+         * @brief Gets the direction of the ray.
+         */
         const glm::dvec3& getDirection() const { return this->_direction; }
 
+        /**
+         * @brief Constructs a new ray with its direction opposite this one.
+         */
         Ray operator-() const;
 
     private:
