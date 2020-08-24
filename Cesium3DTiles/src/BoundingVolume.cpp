f50aaae886423c102ed10900281c64c0632f5a9d
Kevin Ring <kevin@kotachrome.com>
2020-08-24 17:38:04 +1000

    Add BoundingRegionWithLooseFittingHeights, better use of std::variant

--- Cesium3DTiles/src/BoundingVolume.cpp
+++ Cesium3DTiles/src/BoundingVolume.cpp
@@ -7,59 +7,63 @@ using namespace CesiumGeospatial;
 namespace Cesium3DTiles {
 
     BoundingVolume transformBoundingVolume(const glm::dmat4x4& transform, const BoundingVolume& boundingVolume) {
-        switch (boundingVolume.index()) {
-        case 0:
-        {
-            const OrientedBoundingBox& boundingBox = std::get<OrientedBoundingBox>(boundingVolume);
-            glm::dvec3 center = transform * glm::dvec4(boundingBox.getCenter(), 1.0);
-            glm::dmat3 halfAxes = glm::dmat3(transform) * boundingBox.getHalfAxes();
-            return OrientedBoundingBox(center, halfAxes);
-        }
-        case 1:
-        {
-            // Regions are not transformed.
-            return boundingVolume;
-        }
-        case 2:
-        {
-            const BoundingSphere& boundingSphere = std::get<BoundingSphere>(boundingVolume);
-            glm::dvec3 center = transform * glm::dvec4(boundingSphere.getCenter(), 1.0);
+        struct Operation {
+            const glm::dmat4x4& transform;
 
-            double uniformScale = std::max(
-                std::max(
-                    glm::length(glm::dvec3(transform[0])),
-                    glm::length(glm::dvec3(transform[1]))
-                ),
-                glm::length(glm::dvec3(transform[2]))
-            );
+            BoundingVolume operator()(const OrientedBoundingBox& boundingBox) {
+                glm::dvec3 center = transform * glm::dvec4(boundingBox.getCenter(), 1.0);
+                glm::dmat3 halfAxes = glm::dmat3(transform) * boundingBox.getHalfAxes();
+                return OrientedBoundingBox(center, halfAxes);
+            }
 
-            return BoundingSphere(center, boundingSphere.getRadius() * uniformScale);
-        }
-        default:
-            return boundingVolume;
-        }
+            BoundingVolume operator()(const BoundingRegion& boundingRegion) {
+                // Regions are not transformed.
+                return boundingRegion;
+            }
+
+            BoundingVolume operator()(const BoundingSphere& boundingSphere) {
+                glm::dvec3 center = transform * glm::dvec4(boundingSphere.getCenter(), 1.0);
+
+                double uniformScale = std::max(
+                    std::max(
+                        glm::length(glm::dvec3(transform[0])),
+                        glm::length(glm::dvec3(transform[1]))
+                    ),
+                    glm::length(glm::dvec3(transform[2]))
+                );
+
+                return BoundingSphere(center, boundingSphere.getRadius() * uniformScale);
+            }
+
+            BoundingVolume operator()(const BoundingRegionWithLooseFittingHeights& boundingRegion) {
+                // Regions are not transformed.
+                return boundingRegion;
+            }
+        };
+
+        return std::visit(Operation { transform }, boundingVolume);
     }
 
     glm::dvec3 getBoundingVolumeCenter(const BoundingVolume& boundingVolume) {
-        switch (boundingVolume.index()) {
-        case 0:
-        {
-            const OrientedBoundingBox& boundingBox = std::get<OrientedBoundingBox>(boundingVolume);
-            return boundingBox.getCenter();
-        }
-        case 1:
-        {
-            const BoundingRegion& region = std::get<BoundingRegion>(boundingVolume);
-            return region.getBoundingBox().getCenter();
-        }
-        case 2:
-        {
-            const BoundingSphere& boundingSphere = std::get<BoundingSphere>(boundingVolume);
-            return boundingSphere.getCenter();
-        }
-        default:
-            return glm::dvec3(0.0);
-        }
+        struct Operation {
+            glm::dvec3 operator()(const OrientedBoundingBox& boundingBox) {
+                return boundingBox.getCenter();
+            }
+
+            glm::dvec3 operator()(const BoundingRegion& boundingRegion) {
+                return boundingRegion.getBoundingBox().getCenter();
+            }
+
+            glm::dvec3 operator()(const BoundingSphere& boundingSphere) {
+                return boundingSphere.getCenter();
+            }
+
+            glm::dvec3 operator()(const BoundingRegionWithLooseFittingHeights& boundingRegion) {
+                return boundingRegion.getBoundingRegion().getBoundingBox().getCenter();
+            }
+        };
+
+        return std::visit(Operation {}, boundingVolume);
     }
 
 }
---
+++ CesiumGeospatial/src/BoundingRegionWithLooseFittingHeights.cpp
@@ -0,0 +1,2 @@
+#include "CesiumGeospatial/BoundingRegionWithLooseFittingHeights.h"
+
