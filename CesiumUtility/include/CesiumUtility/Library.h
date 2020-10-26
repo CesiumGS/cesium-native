b72787029db44ca14a40ce82370d797a67c511f1
Marco Hutter <javagl@javagl.de>
2020-10-26 23:50:21 +0100

    Documentation for namespaces
    Without them, typedefs of these namespaces did not
    appear in the documentation

--- CesiumUtility/include/CesiumUtility/Library.h
+++ CesiumUtility/include/CesiumUtility/Library.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * @brief Utility classes for Cesium
+ */
+namespace CesiumUtility {}
+
 #if defined(_WIN32) && defined(CESIUM_SHARED)
     #ifdef CESIUMUTILITY_BUILDING
         #define CESIUMUTILITY_API __declspec(dllexport)
