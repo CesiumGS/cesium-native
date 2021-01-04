0d7d7d041432f0f61591884fa2d4d4ff5f7a5363
Kevin Ring <kevin@kotachrome.com>
2021-01-04 13:36:34 +1100

    Add Mesh and Primitve to POC.

--- CesiumGltf/test/TestGltfModel.cpp
+++ CesiumGltf/test/TestGltfModel.cpp
@@ -50,20 +50,20 @@ namespace CesiumGltf {
             this->_depth = 0;
         }
 
-        virtual JsonHandler* Null() { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Bool(bool /*b*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Int(int /*i*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Uint(unsigned /*i*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Int64(int64_t /*i*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Uint64(uint64_t /*i*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* Double(double /*d*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* RawNumber(const char* /*str*/, size_t /*length*/, bool /*copy*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* String(const char* /*str*/, size_t /*length*/, bool /*copy*/) { return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* StartObject() { ++this->_depth; return this; }
-        virtual JsonHandler* Key(const char* /*str*/, size_t /*length*/, bool /*copy*/) { return this; }
-        virtual JsonHandler* EndObject(size_t /*memberCount*/) { --this->_depth; return this->_depth == 0 ? this->parent() : this; }
-        virtual JsonHandler* StartArray() { ++this->_depth; return this; }
-        virtual JsonHandler* EndArray(size_t /*elementCount*/) { --this->_depth; return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Null() override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Bool(bool /*b*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Int(int /*i*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Uint(unsigned /*i*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Int64(int64_t /*i*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Uint64(uint64_t /*i*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* Double(double /*d*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* RawNumber(const char* /*str*/, size_t /*length*/, bool /*copy*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* String(const char* /*str*/, size_t /*length*/, bool /*copy*/) override { return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* StartObject() override { ++this->_depth; return this; }
+        virtual JsonHandler* Key(const char* /*str*/, size_t /*length*/, bool /*copy*/) override { return this; }
+        virtual JsonHandler* EndObject(size_t /*memberCount*/) override { --this->_depth; return this->_depth == 0 ? this->parent() : this; }
+        virtual JsonHandler* StartArray() override { ++this->_depth; return this; }
+        virtual JsonHandler* EndArray(size_t /*elementCount*/) override { --this->_depth; return this->_depth == 0 ? this->parent() : this; }
 
     private:
         size_t _depth = 0;
@@ -76,12 +76,62 @@ namespace CesiumGltf {
         }
     };
 
+    enum class ComponentType {
+        BYTE = 5120,
+        UNSIGNED_BYTE = 5121,
+        SHORT = 5122,
+        UNSIGNED_SHORT = 5123,
+        UNSIGNED_INT = 5125,
+        FLOAT = 5126
+    };
+
+    enum class AccessorType {
+        SCALAR,
+        VEC2,
+        VEC3,
+        VEC4,
+        MAT2,
+        MAT3,
+        MAT4
+    };
+
     struct Accessor {
+        size_t bufferView;
+        size_t byteOffset = 0;
+        ComponentType componentType;
+        bool normalized = false;
         size_t count;
+        AccessorType type;
+        std::vector<double> max;
+        std::vector<double> min;
+    };
+
+    enum class PrimitiveMode {
+        POINTS = 0,
+        LINES = 1,
+        LINE_LOOP = 2,
+        LINE_STRIP = 3,
+        TRIANGLES = 4,
+        TRIANGLE_STRIP = 5,
+        TRIANGLE_FAN = 6
+    };
+
+    struct Primitive {
+        std::unordered_map<std::string, size_t> attributes;
+        size_t indices;
+        size_t material;
+        PrimitiveMode mode = PrimitiveMode::TRIANGLES;
+        std::vector<std::unordered_map<std::string, size_t>> targets;
+    };
+
+    struct Mesh {
+        std::vector<Primitive> primitives;
+        std::vector<double> weights;
     };
 
     struct Model {
         std::vector<Accessor> accessors;
+        std::vector<Mesh> meshes;
     };
 
     template <typename T>
@@ -117,6 +167,49 @@ namespace CesiumGltf {
         T* _pInteger = nullptr;
     };
 
+    class BoolJsonHandler : public JsonHandler {
+    public:
+        void reset(JsonHandler* pParent, bool* pBool) {
+            JsonHandler::reset(pParent);
+            this->_pBool = pBool;
+        }
+
+        virtual JsonHandler* Bool(bool b) override {
+            assert(this->_pBool);
+            *this->_pBool = b;
+            return this->parent();
+        }
+
+    private:
+        bool* _pBool = nullptr;
+    };
+
+    class AccessorTypeJsonHandler : public JsonHandler {
+    public:
+        void reset(JsonHandler* pParent, AccessorType* pEnum) {
+            JsonHandler::reset(pParent);
+            this->_pEnum = pEnum;
+        }
+
+        virtual JsonHandler* String(const char* str, size_t /*length*/, bool /*copy*/) override {
+            using namespace std::string_literals;
+
+            if ("SCALAR"s == str) *this->_pEnum = AccessorType::SCALAR;
+            else if ("VEC2"s == str) *this->_pEnum = AccessorType::VEC2;
+            else if ("VEC3"s == str) *this->_pEnum = AccessorType::VEC3;
+            else if ("VEC4"s == str) *this->_pEnum = AccessorType::VEC4;
+            else if ("MAT2"s == str) *this->_pEnum = AccessorType::MAT2;
+            else if ("MAT3"s == str) *this->_pEnum = AccessorType::MAT3;
+            else if ("MAT4"s == str) *this->_pEnum = AccessorType::MAT4;
+            else return nullptr;
+
+            return this->parent();
+        }
+
+    private:
+        AccessorType* _pEnum = nullptr;
+    };
+
     class ObjectJsonHandler : public JsonHandler {
     public:
         virtual JsonHandler* StartObject() override {
@@ -136,25 +229,40 @@ namespace CesiumGltf {
         IgnoreValueJsonHandler _ignoreHandler;
     };
 
-    class AccessorJsonHandler : public ObjectJsonHandler {
+    class DoubleArrayJsonHandler : public JsonHandler {
     public:
-        void reset(JsonHandler* pParent, Accessor* pAccessor) {
+        void reset(JsonHandler* pParent, std::vector<double>* pArray) {
             JsonHandler::reset(pParent);
-            this->_pAccessor = pAccessor;
+            this->_pArray = pArray;
+            this->_arrayIsOpen = false;
         }
 
-        virtual JsonHandler* Key(const char* str, size_t /*length*/, bool /*copy*/) override {
-            using namespace std::string_literals;
+        virtual JsonHandler* StartArray() override {
+            if (this->_arrayIsOpen) {
+                return nullptr;
+            }
 
-            assert(this->_pAccessor);
-            if ("count"s == str) return property(this->_count, this->_pAccessor->count);
+            this->_arrayIsOpen = true;
+            return this;
+        }
 
-            return this->ignore();
+        virtual JsonHandler* EndArray(size_t) override {
+            return this->parent();
+        }
+
+        virtual JsonHandler* Double(double d) override {
+            if (!this->_arrayIsOpen) {
+                return nullptr;
+            }
+
+            assert(this->_pArray);
+            this->_pArray->emplace_back(d);
+            return this;
         }
 
     private:
-        Accessor* _pAccessor = nullptr;
-        IntegerJsonHandler<size_t> _count;
+        std::vector<double>* _pArray = nullptr;
+        bool _arrayIsOpen = false;
     };
 
     template <typename T, typename THandler>
@@ -197,6 +305,91 @@ namespace CesiumGltf {
         THandler _objectHandler;
     };
 
+    class AccessorJsonHandler : public ObjectJsonHandler {
+    public:
+        void reset(JsonHandler* pParent, Accessor* pAccessor) {
+            JsonHandler::reset(pParent);
+            this->_pAccessor = pAccessor;
+        }
+
+        virtual JsonHandler* Key(const char* str, size_t /*length*/, bool /*copy*/) override {
+            using namespace std::string_literals;
+
+            assert(this->_pAccessor);
+            if ("bufferView"s == str) return property(this->_bufferView, this->_pAccessor->bufferView);
+            if ("byteOffset"s == str) return property(this->_byteOffset, this->_pAccessor->byteOffset);
+            if ("componentType"s == str) return property(this->_componentType, this->_pAccessor->componentType);
+            if ("normalized"s == str) return property(this->_normalized, this->_pAccessor->normalized);
+            if ("count"s == str) return property(this->_count, this->_pAccessor->count);
+            if ("type"s == str) return property(this->_type, this->_pAccessor->type);
+            if ("max"s == str) return property(this->_max, this->_pAccessor->max);
+            if ("min"s == str) return property(this->_min, this->_pAccessor->min);
+
+            return this->ignore();
+        }
+
+    private:
+        Accessor* _pAccessor = nullptr;
+        IntegerJsonHandler<size_t> _bufferView;
+        IntegerJsonHandler<size_t> _byteOffset;
+        IntegerJsonHandler<ComponentType> _componentType;
+        BoolJsonHandler _normalized;
+        IntegerJsonHandler<size_t> _count;
+        AccessorTypeJsonHandler _type;
+        DoubleArrayJsonHandler _max;
+        DoubleArrayJsonHandler _min;
+    };
+
+    class PrimitiveJsonHandler : public ObjectJsonHandler {
+    public:
+        void reset(JsonHandler* pParent, Primitive* pPrimitive) {
+            JsonHandler::reset(pParent);
+            this->_pPrimitive = pPrimitive;
+        }
+
+        virtual JsonHandler* Key(const char* str, size_t /*length*/, bool /*copy*/) override {
+            using namespace std::string_literals;
+
+            assert(this->_pPrimitive);
+            if ("indices"s == str) return property(this->_indices, this->_pPrimitive->indices);
+            if ("material"s == str) return property(this->_material, this->_pPrimitive->material);
+            if ("mode"s == str) return property(this->_mode, this->_pPrimitive->mode);
+
+            return this->ignore();
+        }
+
+    private:
+        Primitive* _pPrimitive = nullptr;
+        // std::unordered_map<std::string, size_t> attributes;
+        IntegerJsonHandler<size_t> _indices;
+        IntegerJsonHandler<size_t> _material;
+        IntegerJsonHandler<PrimitiveMode> _mode;
+        // std::vector<std::unordered_map<std::string, size_t>> targets;
+    };
+
+    class MeshJsonHandler : public ObjectJsonHandler {
+    public:
+        void reset(JsonHandler* pParent, Mesh* pMesh) {
+            JsonHandler::reset(pParent);
+            this->_pMesh = pMesh;
+        }
+
+        virtual JsonHandler* Key(const char* str, size_t /*length*/, bool /*copy*/) override {
+            using namespace std::string_literals;
+
+            assert(this->_pMesh);
+            if ("primitives"s == str) return property(this->_primitives, this->_pMesh->primitives);
+            if ("weights"s == str) return property(this->_weights, this->_pMesh->weights);
+
+            return this->ignore();
+        }
+
+    private:
+        Mesh* _pMesh = nullptr;
+        ObjectArrayJsonHandler<Primitive, PrimitiveJsonHandler> _primitives;
+        DoubleArrayJsonHandler _weights;
+    };
+
     class ModelJsonHandler : public ObjectJsonHandler {
     public:
         void reset(JsonHandler* pParent, Model* pModel) {
@@ -209,6 +402,7 @@ namespace CesiumGltf {
 
             assert(this->_pModel);
             if ("accessors"s == str) return property(this->_accessors, this->_pModel->accessors);
+            if ("meshes"s == str) return property(this->_meshes, this->_pModel->meshes);
 
             return this->ignore();
         }
@@ -216,6 +410,7 @@ namespace CesiumGltf {
     private:
         Model* _pModel;
         ObjectArrayJsonHandler<Accessor, AccessorJsonHandler> _accessors;
+        ObjectArrayJsonHandler<Mesh, MeshJsonHandler> _meshes;
     };
 
     class GltfReader final {
@@ -233,8 +428,9 @@ namespace CesiumGltf {
 
             reader.IterativeParseInit();
 
-            while (!reader.IterativeParseComplete()) {
-                reader.IterativeParseNext<rapidjson::kParseDefaultFlags>(inputStream, dispatcher);
+            bool success = true;
+            while (success && !reader.IterativeParseComplete()) {
+                success = reader.IterativeParseNext<rapidjson::kParseDefaultFlags>(inputStream, dispatcher);
             }
 
             return result;
@@ -476,12 +672,21 @@ namespace CesiumGltf {
 }
 
 TEST_CASE("GltfModel") {
-    std::string s = "{\"accessors\": [{\"count\": 4}]}";
+    std::string s = "{\"accessors\": [{\"count\": 4,\"componentType\":5121,\"type\":\"VEC2\",\"max\":[1.0, 2.2, 3.3],\"min\":[0.0, -1.2]}],\"surprise\":{\"foo\":true}}";
     GltfReader reader;
     
     Model model = reader.parse(gsl::span(reinterpret_cast<const uint8_t*>(s.c_str()), s.size()));
-    CHECK(model.accessors.size() == 1);
+    REQUIRE(model.accessors.size() == 1);
     CHECK(model.accessors[0].count == 4);
+    CHECK(model.accessors[0].componentType == ComponentType::UNSIGNED_BYTE);
+    CHECK(model.accessors[0].type == AccessorType::VEC2);
+    REQUIRE(model.accessors[0].min.size() == 2);
+    CHECK(model.accessors[0].min[0] == 0.0);
+    CHECK(model.accessors[0].min[1] == -1.2);
+    REQUIRE(model.accessors[0].max.size() == 3);
+    CHECK(model.accessors[0].max[0] == 1.0);
+    CHECK(model.accessors[0].max[1] == 2.2);
+    CHECK(model.accessors[0].max[2] == 3.3);
     // std::vector<uint8_t> v;
     // GltfModel model = GltfModel::fromMemory(v);
     // GltfCollection<GltfMesh> meshes = model.meshes();
