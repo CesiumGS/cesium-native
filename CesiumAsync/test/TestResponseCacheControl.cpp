3b5074bef275807335e40f44fcdab2f9aa832ebd
unknown <tranthienbao123@gmail.com>
2021-01-27 15:19:41 -0500

    test parse response cache control with only some directive

--- CesiumAsync/test/TestResponseCacheControl.cpp
+++ CesiumAsync/test/TestResponseCacheControl.cpp
@@ -30,5 +30,23 @@ TEST_CASE("Test parsing cache-control header") {
 		REQUIRE(cacheControl->maxAge() == 1000);
 		REQUIRE(cacheControl->sharedMaxAge() == 10);
 	}
+
+	SECTION("Header has cache-control header with only some directive") {
+		HttpHeaders responseHeader{
+			{"Cache-Control", "Must-Revalidate, No-Cache, No-Store, Public, Private, Max-Age = 1000,  S-Maxage = 10"},
+		};
+
+		std::optional<ResponseCacheControl> cacheControl = ResponseCacheControl::parseFromResponseHeaders(responseHeader);
+		REQUIRE(cacheControl != std::nullopt);
+		REQUIRE(cacheControl->mustRevalidate());
+		REQUIRE(cacheControl->noCache());
+		REQUIRE(cacheControl->noStore());
+		REQUIRE(cacheControl->noTransform() == false);
+		REQUIRE(cacheControl->accessControlPublic());
+		REQUIRE(cacheControl->accessControlPrivate());
+		REQUIRE(cacheControl->proxyRevalidate() == false);
+		REQUIRE(cacheControl->maxAge() == 1000);
+		REQUIRE(cacheControl->sharedMaxAge() == 10);
+	}
 }
 
