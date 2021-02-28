1a62ae6dc4929656f26912429b35534c87a466d7
Kevin Ring <kevin@kotachrome.com>
2021-02-28 23:10:53 +1100

    Fix Clang and GCC errors.

--- CesiumIonClient/src/Connection.cpp
+++ CesiumIonClient/src/Connection.cpp
@@ -12,10 +12,16 @@
 #include <rapidjson/writer.h>
 #include <thread>
 
+#ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable:4244)
+#endif
+
 #include <picosha2.h>
+
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 
 using namespace CesiumAsync;
 using namespace CesiumIonClient;
