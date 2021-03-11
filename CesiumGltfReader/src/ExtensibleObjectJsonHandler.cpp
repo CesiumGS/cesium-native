eb3cb751bfac7ac336c02cb24e6b2f3e99a4efaa
Samuel Vargas <sam.vargas@cesium.com>
2021-03-11 09:41:03 -0500

    Always serialize extensions as a JsonValue::Object for clients to use directly.
    (This will be wrapped in a conditional flag in the next commit)

--- CesiumGltfReader/src/ExtensibleObjectJsonHandler.cpp
+++ CesiumGltfReader/src/ExtensibleObjectJsonHandler.cpp
@@ -17,5 +17,10 @@ IJsonHandler* ExtensibleObjectJsonHandler::ExtensibleObjectKey(
   if ("extras"s == str)
     return property("extras", this->_extras, o.extras);
 
+  if ("extensions"s == str) {
+    o.extensions.emplace_back(JsonValue::Object());
+    return property("extensions", this->_extensions, std::any_cast<JsonValue::Object&>(o.extensions.back()));
+  }
+
   return this->ignoreAndContinue();
 }
