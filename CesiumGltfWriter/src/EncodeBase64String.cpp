29f3f9f07eeaf215b08cabd18c61fc012fe0a97b
Samuel Vargas <sam.vargas@cesium.com>
2021-04-19 17:18:11 -0400

    Add noexcept back to encodeAsBase64String

--- CesiumGltfWriter/src/EncodeBase64String.cpp
+++ CesiumGltfWriter/src/EncodeBase64String.cpp
@@ -2,7 +2,7 @@
 #include <modp_b64.h>
 
 std::string
-CesiumGltf::encodeAsBase64String(const std::vector<std::byte>& data) {
+CesiumGltf::encodeAsBase64String(const std::vector<std::byte>& data) noexcept {
   const std::size_t len = modp_b64_encode_len(data.size());
   std::vector<char> destination(len, 0);
   const char* asCharPointer = reinterpret_cast<const char*>(data.data());
