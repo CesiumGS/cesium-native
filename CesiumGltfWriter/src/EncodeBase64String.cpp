91c7b7f66324d533b968f0ba042a60f464cafdbb
Samuel Vargas <sam.vargas@cesium.com>
2021-04-21 15:18:42 -0400

    Rewrite encodeAsBase64String to avoid extra copy
    Also add additional unittests for the class.

--- CesiumGltfWriter/src/EncodeBase64String.cpp
+++ CesiumGltfWriter/src/EncodeBase64String.cpp
@@ -3,15 +3,11 @@
 
 std::string
 CesiumGltf::encodeAsBase64String(const std::vector<std::byte>& data) noexcept {
-  const std::size_t len = modp_b64_encode_len(data.size());
-  std::vector<char> destination(len, 0);
+  const std::size_t paddedLength = modp_b64_encode_len(data.size());
+  std::string result(paddedLength, '\0');
   const char* asCharPointer = reinterpret_cast<const char*>(data.data());
-  modp_b64_encode(destination.data(), asCharPointer, data.size());
-
-  // TODO: Returning std:string(destination.begin(), destination.end())
-  //       adds *ALL* the padding bytes instead of just stopping at the first
-  //       null byte. It seems more correct to do that, however, if I do that
-  //       buffer.cesium.data is initalized to nothing when CesiumGltfReader
-  //       reads the glTF asset back in. Bug?
-  return std::string(destination.data());
+  const std::size_t actualLength =
+      modp_b64_encode(result.data(), asCharPointer, data.size());
+  result.erase(actualLength, std::string::npos);
+  return result;
 }
