4c7a43141634f2ebd32d324e5381a322a06f2a9e
Bao Tran <tranthienbao123@gmail.com>
2021-05-18 15:52:22 -0400

    re-arrange buffer for b3dm batch table buffer to match EXT_feature_table

--- Cesium3DTiles/src/upgradeBatchTableToFeatureMetadata.cpp
+++ Cesium3DTiles/src/upgradeBatchTableToFeatureMetadata.cpp
@@ -9,146 +9,6 @@
 using namespace CesiumGltf;
 
 namespace {
-
-void updateExtensionWithProperty(
-    Model& gltf,
-    int64_t binaryBufferIndex,
-    ClassProperty& classProperty,
-    FeatureTable& featureTable,
-    FeatureTableProperty& featureTableProperty,
-    const rapidjson::Value& propertyValue);
-void updateExtensionWithJsonProperty(
-    Model& gltf,
-    ClassProperty& classProperty,
-    FeatureTable& featureTable,
-    FeatureTableProperty& featureTableProperty,
-    const rapidjson::Value& propertyValue);
-void updateExtensionWithBinaryProperty(
-    Model& gltf,
-    int64_t binaryBufferIndex,
-    ClassProperty& classProperty,
-    FeatureTable& featureTable,
-    FeatureTableProperty& featureTableProperty,
-    const rapidjson::Value& propertyValue);
-
-} // namespace
-
-namespace Cesium3DTiles {
-
-void upgradeBatchTableToFeatureMetadata(
-    const std::shared_ptr<spdlog::logger>& pLogger,
-    CesiumGltf::Model& gltf,
-    const rapidjson::Document& featureTableJson,
-    const gsl::span<const std::byte>& batchTableJsonData,
-    const gsl::span<const std::byte>& batchTableBinaryData) {
-
-  // Parse the b3dm batch table and convert it to the EXT_feature_metadata
-  // extension.
-
-  // If the feature table is missing the BATCH_LENGTH semantic, ignore the batch
-  // table completely.
-  auto batchLengthIt = featureTableJson.FindMember("BATCH_LENGTH");
-  if (batchLengthIt == featureTableJson.MemberEnd() ||
-      !batchLengthIt->value.IsInt64()) {
-    SPDLOG_LOGGER_WARN(
-        pLogger,
-        "The B3DM has a batch table, but it is being ignored because there is "
-        "no BATCH_LENGTH semantic in the feature table or it is not an "
-        "integer.");
-    return;
-  }
-
-  int64_t batchLength = batchLengthIt->value.GetInt64();
-
-  rapidjson::Document document;
-  document.Parse(
-      reinterpret_cast<const char*>(batchTableJsonData.data()),
-      batchTableJsonData.size());
-  if (document.HasParseError()) {
-    SPDLOG_LOGGER_ERROR(
-        pLogger,
-        "Error when parsing batch table JSON, error code {} at byte offset "
-        "{}",
-        document.GetParseError(),
-        document.GetErrorOffset());
-    return;
-  }
-
-  // Add the binary part of the batch table - if any - to the glTF as a buffer.
-  int64_t binaryBufferIndex = -1;
-
-  if (!batchTableBinaryData.empty()) {
-    binaryBufferIndex = int64_t(gltf.buffers.size());
-    Buffer& binaryBuffer = gltf.buffers.emplace_back();
-    binaryBuffer.byteLength = batchTableBinaryData.size();
-    binaryBuffer.cesium.data = std::vector<std::byte>(
-        batchTableBinaryData.begin(),
-        batchTableBinaryData.end());
-  }
-
-  ModelEXT_feature_metadata& modelExtension =
-      gltf.addExtension<ModelEXT_feature_metadata>();
-  Schema& schema = modelExtension.schema.emplace();
-  Class& classDefinition =
-      schema.classes.emplace("default", Class()).first->second;
-
-  FeatureTable& featureTable =
-      modelExtension.featureTables.emplace("default", FeatureTable())
-          .first->second;
-  featureTable.count = batchLength;
-  featureTable.classProperty = "default";
-
-  // Convert each regular property in the batch table
-  for (auto propertyIt = document.MemberBegin();
-       propertyIt != document.MemberEnd();
-       ++propertyIt) {
-    std::string name = propertyIt->name.GetString();
-    ClassProperty& classProperty =
-        classDefinition.properties.emplace(name, ClassProperty()).first->second;
-    classProperty.name = name;
-
-    FeatureTableProperty& featureTableProperty =
-        featureTable.properties.emplace(name, FeatureTableProperty())
-            .first->second;
-
-    updateExtensionWithProperty(
-        gltf,
-        binaryBufferIndex,
-        classProperty,
-        featureTable,
-        featureTableProperty,
-        propertyIt->value);
-  }
-
-  // Create an EXT_feature_metadata extension for each primitive with a _BATCHID
-  // attribute.
-  for (Mesh& mesh : gltf.meshes) {
-    for (MeshPrimitive& primitive : mesh.primitives) {
-      auto batchIDIt = primitive.attributes.find("_BATCHID");
-      if (batchIDIt == primitive.attributes.end()) {
-        // This primitive has no batch ID, ignore it.
-        continue;
-      }
-
-      // Rename the _BATCHID attribute to _FEATURE_ID_0
-      primitive.attributes["_FEATURE_ID_0"] = batchIDIt->second;
-      primitive.attributes.erase("_BATCHID");
-
-      // Create a feature extension
-      MeshPrimitiveEXT_feature_metadata& extension =
-          primitive.addExtension<MeshPrimitiveEXT_feature_metadata>();
-      FeatureIDAttribute& attribute =
-          extension.featureIdAttributes.emplace_back();
-      attribute.featureTable = "default";
-      attribute.featureIds.attribute = "_FEATURE_ID_0";
-    }
-  }
-}
-
-} // namespace Cesium3DTiles
-
-namespace {
-
 struct CompatibleTypes {
   bool isInt8 = true;
   bool isUint8 = true;
@@ -161,6 +21,12 @@ struct CompatibleTypes {
   bool isFloat32 = true;
   bool isFloat64 = true;
   bool isBool = true;
+};
+
+struct BinaryProperty {
+  int64_t b3dmByteOffset;
+  int64_t gltfByteOffset;
+  int64_t byteLength;
 };
 
 struct GltfFeatureTableType {
@@ -180,30 +46,8 @@ const std::map<std::string, GltfFeatureTableType> b3dmComponentTypeToGltfType =
         {"DOUBLE", GltfFeatureTableType{"FLOAT64", sizeof(double)}},
 };
 
-void updateExtensionWithProperty(
-    Model& gltf,
-    int64_t binaryBufferIndex,
-    ClassProperty& classProperty,
-    FeatureTable& featureTable,
-    FeatureTableProperty& featureTableProperty,
-    const rapidjson::Value& propertyValue) {
-
-  if (propertyValue.IsArray()) {
-    updateExtensionWithJsonProperty(
-        gltf,
-        classProperty,
-        featureTable,
-        featureTableProperty,
-        propertyValue);
-  } else {
-    updateExtensionWithBinaryProperty(
-        gltf,
-        binaryBufferIndex,
-        classProperty,
-        featureTable,
-        featureTableProperty,
-        propertyValue);
-  }
+int64_t roundUp(int64_t num, int64_t multiple) {
+  return ((num + multiple - 1) / multiple) * multiple;
 }
 
 void updateExtensionWithJsonStringProperty(
@@ -463,18 +307,20 @@ void updateExtensionWithJsonProperty(
 
 void updateExtensionWithBinaryProperty(
     Model& gltf,
-    int64_t binaryBufferIndex,
+    int32_t gltfBufferIndex,
+    int64_t gltfBufferOffset,
+    BinaryProperty &binaryProperty,
     ClassProperty& classProperty,
     FeatureTable& featureTable,
     FeatureTableProperty& featureTableProperty,
     const rapidjson::Value& propertyValue) {
-  if (binaryBufferIndex < 0) {
+  if (gltfBufferIndex < 0) {
     return;
   }
 
   const auto& byteOffsetIt = propertyValue.FindMember("byteOffset");
   if (byteOffsetIt == propertyValue.MemberEnd() ||
-      !byteOffsetIt->value.IsInt()) {
+      !byteOffsetIt->value.IsInt64()) {
     return;
   }
 
@@ -490,7 +336,7 @@ void updateExtensionWithBinaryProperty(
   }
 
   // convert class property
-  int32_t byteOffset = byteOffsetIt->value.GetInt();
+  int64_t byteOffset = byteOffsetIt->value.GetInt64();
   const std::string& componentType = componentTypeIt->value.GetString();
   const std::string& type = typeIt->value.GetString();
 
@@ -521,13 +367,156 @@ void updateExtensionWithBinaryProperty(
   // convert feature table
   size_t typeSize = gltfType.typeSize;
   auto& bufferView = gltf.bufferViews.emplace_back();
-  bufferView.buffer = static_cast<int32_t>(binaryBufferIndex);
-  bufferView.byteOffset = byteOffset;
+  bufferView.buffer = gltfBufferIndex;
+  bufferView.byteOffset = gltfBufferOffset;
   bufferView.byteStride = typeSize;
   bufferView.byteLength = typeSize * featureTable.count;
 
   featureTableProperty.bufferView =
       static_cast<int32_t>(gltf.bufferViews.size() - 1);
+
+  binaryProperty.b3dmByteOffset = byteOffset;
+  binaryProperty.gltfByteOffset = gltfBufferOffset;
+  binaryProperty.byteLength = static_cast<int64_t>(bufferView.byteLength);
+}
+} // namespace
+
+namespace Cesium3DTiles {
+
+void upgradeBatchTableToFeatureMetadata(
+    const std::shared_ptr<spdlog::logger>& pLogger,
+    CesiumGltf::Model& gltf,
+    const rapidjson::Document& featureTableJson,
+    const gsl::span<const std::byte>& batchTableJsonData,
+    const gsl::span<const std::byte>& batchTableBinaryData) {
+
+  // Parse the b3dm batch table and convert it to the EXT_feature_metadata
+  // extension.
+
+  // If the feature table is missing the BATCH_LENGTH semantic, ignore the batch
+  // table completely.
+  auto batchLengthIt = featureTableJson.FindMember("BATCH_LENGTH");
+  if (batchLengthIt == featureTableJson.MemberEnd() ||
+      !batchLengthIt->value.IsInt64()) {
+    SPDLOG_LOGGER_WARN(
+        pLogger,
+        "The B3DM has a batch table, but it is being ignored because there is "
+        "no BATCH_LENGTH semantic in the feature table or it is not an "
+        "integer.");
+    return;
+  }
+
+  int64_t batchLength = batchLengthIt->value.GetInt64();
+
+  rapidjson::Document document;
+  document.Parse(
+      reinterpret_cast<const char*>(batchTableJsonData.data()),
+      batchTableJsonData.size());
+  if (document.HasParseError()) {
+    SPDLOG_LOGGER_ERROR(
+        pLogger,
+        "Error when parsing batch table JSON, error code {} at byte offset "
+        "{}",
+        document.GetParseError(),
+        document.GetErrorOffset());
+    return;
+  }
+
+  // Add the binary part of the batch table - if any - to the glTF as a buffer.
+  // We will reallign this buffer later on
+  int32_t gltfBufferIndex = -1;
+  int64_t gltfBufferOffset = -1;
+  std::vector<BinaryProperty> binaryProperties;
+  if (!batchTableBinaryData.empty()) {
+    gltfBufferIndex = static_cast<int32_t>(gltf.buffers.size());
+    gltfBufferOffset = 0;
+    gltf.buffers.emplace_back();
+  }
+
+  ModelEXT_feature_metadata& modelExtension =
+      gltf.addExtension<ModelEXT_feature_metadata>();
+  Schema& schema = modelExtension.schema.emplace();
+  Class& classDefinition =
+      schema.classes.emplace("default", Class()).first->second;
+
+  FeatureTable& featureTable =
+      modelExtension.featureTables.emplace("default", FeatureTable())
+          .first->second;
+  featureTable.count = batchLength;
+  featureTable.classProperty = "default";
+
+  // Convert each regular property in the batch table
+  for (auto propertyIt = document.MemberBegin();
+       propertyIt != document.MemberEnd();
+       ++propertyIt) {
+    std::string name = propertyIt->name.GetString();
+    ClassProperty& classProperty =
+        classDefinition.properties.emplace(name, ClassProperty()).first->second;
+    classProperty.name = name;
+
+    FeatureTableProperty& featureTableProperty =
+        featureTable.properties.emplace(name, FeatureTableProperty())
+            .first->second;
+    const rapidjson::Value& propertyValue = propertyIt->value;
+    if (propertyValue.IsArray()) {
+      updateExtensionWithJsonProperty(
+          gltf,
+          classProperty,
+          featureTable,
+          featureTableProperty,
+          propertyValue);
+    } else {
+      BinaryProperty& binaryProperty = binaryProperties.emplace_back();
+      updateExtensionWithBinaryProperty(
+          gltf,
+          gltfBufferIndex,
+          gltfBufferOffset,
+          binaryProperty,
+          classProperty,
+          featureTable,
+          featureTableProperty,
+          propertyValue);
+      gltfBufferOffset += roundUp(binaryProperty.byteLength, 8);
+    }
+  }
+
+  // re-arrange binary property buffer
+  if (!batchTableBinaryData.empty()) {
+    Buffer& buffer = gltf.buffers[gltfBufferIndex];
+    buffer.byteLength = gltfBufferOffset;
+    buffer.cesium.data.resize(gltfBufferOffset);
+    for (const BinaryProperty& binaryProperty : binaryProperties) {
+      std::memcpy(
+          buffer.cesium.data.data() + binaryProperty.gltfByteOffset,
+          batchTableBinaryData.data() + binaryProperty.b3dmByteOffset,
+          binaryProperty.byteLength);
+    }
+  }
+
+  // Create an EXT_feature_metadata extension for each primitive with a _BATCHID
+  // attribute.
+  for (Mesh& mesh : gltf.meshes) {
+    for (MeshPrimitive& primitive : mesh.primitives) {
+      auto batchIDIt = primitive.attributes.find("_BATCHID");
+      if (batchIDIt == primitive.attributes.end()) {
+        // This primitive has no batch ID, ignore it.
+        continue;
+      }
+
+      // Rename the _BATCHID attribute to _FEATURE_ID_0
+      primitive.attributes["_FEATURE_ID_0"] = batchIDIt->second;
+      primitive.attributes.erase("_BATCHID");
+
+      // Create a feature extension
+      MeshPrimitiveEXT_feature_metadata& extension =
+          primitive.addExtension<MeshPrimitiveEXT_feature_metadata>();
+      FeatureIDAttribute& attribute =
+          extension.featureIdAttributes.emplace_back();
+      attribute.featureTable = "default";
+      attribute.featureIds.attribute = "_FEATURE_ID_0";
+    }
+  }
 }
 
-} // namespace
+} // namespace Cesium3DTiles
+
