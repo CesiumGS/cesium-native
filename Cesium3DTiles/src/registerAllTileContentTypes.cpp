a1d180de5227a0a9ad433e04d0d02d40141f5de8
Nithin Pranesh <nithinpra7@gmail.com>
2021-06-03 14:13:36 -0400

    initial set up of i3dm parser

--- Cesium3DTiles/src/registerAllTileContentTypes.cpp
+++ Cesium3DTiles/src/registerAllTileContentTypes.cpp
@@ -1,5 +1,6 @@
 #include "Cesium3DTiles/registerAllTileContentTypes.h"
 #include "Batched3DModelContent.h"
+#include "Instanced3DModelContent.h"
 #include "Cesium3DTiles/ExternalTilesetContent.h"
 #include "Cesium3DTiles/GltfContent.h"
 #include "Cesium3DTiles/TileContentFactory.h"
@@ -11,6 +12,9 @@ namespace Cesium3DTiles {
 void registerAllTileContentTypes() {
 
   TileContentFactory::registerMagic("glTF", std::make_shared<GltfContent>());
+  TileContentFactory::registerMagic(
+      "i3dm", 
+      std::make_shared<Instanced3DModelContent>());
   TileContentFactory::registerMagic(
       "b3dm",
       std::make_shared<Batched3DModelContent>());
