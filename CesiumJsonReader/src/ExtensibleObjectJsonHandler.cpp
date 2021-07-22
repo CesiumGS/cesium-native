7cddd127d597a7fed489c0758870ff431b92916b
Daniel Krupka <daniel@cesium.com>
2021-07-22 15:38:49 -0400

    Add factored-out extension handling

---
+++ CesiumJsonReader/src/ExtensibleObjectJsonHandler.cpp
@@ -0,0 +1,34 @@
+#include "CesiumJsonReader/ExtensibleObjectJsonHandler.h"
+#include "CesiumJsonReader/ExtensibleObject.h"
+#include "CesiumJsonReader/ExtensionsJsonHandler.h"
+#include "CesiumJsonReader/JsonHandler.h"
+#include "CesiumJsonReader/ObjectJsonHandler.h"
+
+using namespace CesiumJsonReader;
+
+ExtensibleObjectJsonHandler::ExtensibleObjectJsonHandler(
+    const ExtensionContext& context) noexcept
+    : ObjectJsonHandler(), _extras(), _extensions(context) {}
+
+void ExtensibleObjectJsonHandler::reset(
+    IJsonHandler* pParent,
+    ExtensibleObject* /*pObject*/) {
+  ObjectJsonHandler::reset(pParent);
+}
+
+IJsonHandler* ExtensibleObjectJsonHandler::readObjectKeyExtensibleObject(
+    const std::string& objectType,
+    const std::string_view& str,
+    ExtensibleObject& o) {
+  using namespace std::string_literals;
+
+  if ("extras"s == str)
+    return property("extras", this->_extras, o.extras);
+
+  if ("extensions"s == str) {
+    this->_extensions.reset(this, &o, objectType);
+    return &this->_extensions;
+  }
+
+  return this->ignoreAndContinue();
+}
---
+++ CesiumJsonReader/src/ExtensionsJsonHandler.cpp
@@ -0,0 +1,28 @@
+#include "CesiumJsonReader/ExtensionsJsonHandler.h"
+#include "CesiumJsonReader/ExtensionContext.h"
+
+using namespace CesiumJsonReader;
+
+void ExtensionsJsonHandler::reset(
+    IJsonHandler* pParent,
+    ExtensibleObject* pObject,
+    const std::string& objectType) {
+  ObjectJsonHandler::reset(pParent);
+  this->_pObject = pObject;
+
+  if (this->_objectType != objectType) {
+    this->_objectType = objectType;
+  }
+}
+
+IJsonHandler*
+ExtensionsJsonHandler::readObjectKey(const std::string_view& str) {
+  this->_currentExtensionHandler =
+      this->_context.createExtensionHandler(str, this->_objectType);
+  if (this->_currentExtensionHandler) {
+    this->_currentExtensionHandler->reset(this, *this->_pObject, str);
+    return this->_currentExtensionHandler.get();
+  } else {
+    return this->ignoreAndContinue();
+  }
+}
