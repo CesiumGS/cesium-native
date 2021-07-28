9b7539d0133fee39f3e4f6c9c81c3d053dcbc312
Kevin Ring <kevin@kotachrome.com>
2021-07-28 15:19:52 +1000

    PropertyType doesn't need to be a bitmask.

--- CesiumGltf/include/CesiumGltf/PropertyType.h
+++ CesiumGltf/include/CesiumGltf/PropertyType.h
@@ -6,21 +6,21 @@
 
 namespace CesiumGltf {
 enum class PropertyType {
-  None = 0,
-  Int8 = 1 << 0,
-  Uint8 = 1 << 1,
-  Int16 = 1 << 2,
-  Uint16 = 1 << 3,
-  Int32 = 1 << 4,
-  Uint32 = 1 << 5,
-  Int64 = 1 << 6,
-  Uint64 = 1 << 7,
-  Float32 = 1 << 8,
-  Float64 = 1 << 9,
-  Boolean = 1 << 10,
-  Enum = 1 << 11,
-  String = 1 << 12,
-  Array = 1 << 13,
+  None,
+  Int8,
+  Uint8,
+  Int16,
+  Uint16,
+  Int32,
+  Uint32,
+  Int64,
+  Uint64,
+  Float32,
+  Float64,
+  Boolean,
+  Enum,
+  String,
+  Array,
 };
 
 std::string convertPropertyTypeToString(CesiumGltf::PropertyType type);
