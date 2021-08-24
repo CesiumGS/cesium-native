7d54cd067e5aded4dc61c1b2e4eb952ce611d5ff
Kevin Ring <kevin@kotachrome.com>
2021-08-24 12:14:12 +1000

    Revert "Remove unnecessary AsyncSystem-related changes."
    This reverts commit c7795573e35ef7c1b8a20b6a8f9c5346576ef069.

--- CesiumAsync/include/CesiumAsync/Impl/ContinuationReturnType.h
+++ CesiumAsync/include/CesiumAsync/Impl/ContinuationReturnType.h
@@ -3,11 +3,29 @@
 #include <type_traits>
 
 namespace CesiumAsync {
+
+template <typename T> class SharedFutureResult;
+
 namespace Impl {
 // Begin omitting doxgen warnings for Impl namespace
 //! @cond Doxygen_Suppress
 
-template <typename Func, typename T> struct ContinuationReturnType {
+template <typename Func, typename T, typename Enable = void>
+struct ContinuationReturnType {};
+
+template <typename Func, typename T>
+struct ContinuationReturnType<
+    Func,
+    T,
+    std::enable_if_t<std::is_invocable_v<Func, SharedFutureResult<T>>>> {
+  using type = typename std::invoke_result<Func, SharedFutureResult<T>>::type;
+};
+
+template <typename Func, typename T>
+struct ContinuationReturnType<
+    Func,
+    T,
+    std::enable_if_t<std::is_invocable_v<Func, T>>> {
   using type = typename std::invoke_result<Func, T>::type;
 };
 
--- CesiumAsync/include/CesiumAsync/Impl/unwrapFuture.h
+++ CesiumAsync/include/CesiumAsync/Impl/unwrapFuture.h
@@ -2,21 +2,58 @@
 
 #include "CesiumAsync/Impl/ContinuationFutureType.h"
 #include "CesiumAsync/Impl/ContinuationReturnType.h"
+#include "CesiumAsync/SharedFutureResult.h"
+#include <type_traits>
 
 namespace CesiumAsync {
 namespace Impl {
 // Begin omitting doxgen warnings for Impl namespace
 //! @cond Doxygen_Suppress
 
+template <typename T>
+inline constexpr bool isFuture =
+    !std::is_same_v<T, typename RemoveFuture<T>::type>;
+
+template <typename Func, typename T>
+auto futureFunctionToTaskFunction(Func&& f) {
+  if constexpr (std::is_invocable_v<Func>) {
+    // Function taking no parameters
+    if constexpr (isFuture<typename ContinuationReturnType<Func, void>::type>) {
+      // And returning a Future
+      return [f = std::forward<Func>(f)]() { return f()._task; };
+    } else {
+      // And returning a regular value
+      return std::forward<Func>(f);
+    }
+  } else if constexpr (std::is_invocable_v<Func, SharedFutureResult<T>>) {
+    // Function taking a SharedFuture<T>
+    if constexpr (isFuture<typename ContinuationReturnType<Func, T>::type>) {
+      // And returning a Future
+      return [f = std::forward<Func>(f)](const async::shared_task<T>& task) {
+        return f(SharedFutureResult<T>(task))._task;
+      };
+    } else {
+      // And returning a regular value
+      return [f = std::forward<Func>(f)](const async::shared_task<T>& task) {
+        return f(SharedFutureResult<T>(task));
+      };
+    }
+  } else {
+    // Function taking a normal value T
+    if constexpr (isFuture<typename ContinuationReturnType<Func, T>::type>) {
+      // And returning a Future
+      return
+          [f = std::forward<Func>(f)](T&& t) { return f(std::move(t))._task; };
+    } else {
+      // And returning a regular value
+      return std::forward<Func>(f);
+    }
+  }
+}
+
 struct IdentityUnwrapper {
   template <typename Func> static Func unwrap(Func&& f) {
     return std::forward<Func>(f);
-  }
-};
-
-template <typename T> struct ParameterizedTaskUnwrapper {
-  template <typename Func> static auto unwrap(Func&& f) {
-    return [f = std::forward<Func>(f)](T&& t) { return f(std::move(t))._task; };
   }
 };
 
@@ -27,13 +64,7 @@ struct TaskUnwrapper {
 };
 
 template <typename Func, typename T> auto unwrapFuture(Func&& f) {
-  return std::conditional<
-      std::is_same<
-          typename ContinuationReturnType<Func, T>::type,
-          typename RemoveFuture<
-              typename ContinuationFutureType<Func, T>::type>::type>::value,
-      IdentityUnwrapper,
-      ParameterizedTaskUnwrapper<T>>::type::unwrap(std::forward<Func>(f));
+  return futureFunctionToTaskFunction<Func, T>(std::forward<Func>(f));
 }
 
 template <typename Func> auto unwrapFuture(Func&& f) {
