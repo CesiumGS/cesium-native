4f5f4f6503351968b1fe44b778c308bc63afee93
Sean Lilley <lilleyse@gmail.com>
2021-10-18 19:30:56 -0400

    Remove unneeded code in TilesetWriter.h

--- Cesium3DTilesWriter/generated/TilesetWriter.h
+++ Cesium3DTilesWriter/generated/TilesetWriter.h
@@ -9,48 +9,36 @@
 namespace Cesium3DTiles {
 
 struct AssetWriter {
-  using ValueType = Asset;
-
   static void write(
       const Asset& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
       const CesiumJsonWriter::ExtensionWriterContext& context);
 };
 struct BoundingVolumeWriter {
-  using ValueType = BoundingVolume;
-
   static void write(
       const BoundingVolume& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
       const CesiumJsonWriter::ExtensionWriterContext& context);
 };
 struct ContentWriter {
-  using ValueType = Content;
-
   static void write(
       const Content& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
       const CesiumJsonWriter::ExtensionWriterContext& context);
 };
 struct TileWriter {
-  using ValueType = Tile;
-
   static void write(
       const Tile& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
       const CesiumJsonWriter::ExtensionWriterContext& context);
 };
 struct PropertiesWriter {
-  using ValueType = Properties;
-
   static void write(
       const Properties& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
       const CesiumJsonWriter::ExtensionWriterContext& context);
 };
 struct TilesetWriter {
-  using ValueType = Tileset;
-
   static void write(
       const Tileset& obj,
       CesiumJsonWriter::JsonWriter& jsonWriter,
