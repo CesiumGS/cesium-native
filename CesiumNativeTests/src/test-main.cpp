a30e07011ccffc3d039793f7914b338a7dcd2984
Kevin Ring <kevin@kotachrome.com>
2021-11-16 21:07:38 +1100

    Add Perfetto, but it doesn't compile.

--- CesiumNativeTests/src/test-main.cpp
+++ CesiumNativeTests/src/test-main.cpp
@@ -1,2 +1,48 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+
+#include "trace_categories.h"
+
+// #ifdef _MSC_VER
+// #pragma warning(push)
+// #pragma warning(disable : 4127)
+
+// #define NOMINMAX
+// #define WIN32_LEAN_AND_MEAN
+// #endif
+
+// #include <perfetto.h>
+
+// #ifdef _MSC_VER
+// #pragma warning(pop)
+// #endif
+
+// // The set of track event categories that the example is using.
+// PERFETTO_DEFINE_CATEGORIES(
+//     perfetto::Category("rendering")
+//         .SetDescription("Rendering and graphics events"),
+//     perfetto::Category("network.debug")
+//         .SetTags("debug")
+//         .SetDescription("Verbose network events"),
+//     perfetto::Category("audio.latency")
+//         .SetTags("verbose")
+//         .SetDescription("Detailed audio latency metrics"));
+
+// PERFETTO_TRACK_EVENT_STATIC_STORAGE();
+
+int main(int argc, char* argv[]) {
+  perfetto::TracingInitArgs args;
+  // The backends determine where trace events are recorded. For this example we
+  // are going to use the in-process tracing service, which only includes in-app
+  // events.
+  args.backends = perfetto::kInProcessBackend;
+
+  perfetto::Tracing::Initialize(args);
+  perfetto::TrackEvent::Register();
+
+  int result = Catch::Session().run(argc, argv);
+
+  // global clean-up...
+
+  return result;
+}
---
+++ CesiumNativeTests/src/trace_categories.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TRACE_CATEGORIES_H
+#define TRACE_CATEGORIES_H
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4127)
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <perfetto.h>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+// The set of track event categories that the example is using.
+PERFETTO_DEFINE_CATEGORIES(
+    perfetto::Category("rendering")
+        .SetDescription("Rendering and graphics events"),
+    perfetto::Category("network.debug")
+        .SetTags("debug")
+        .SetDescription("Verbose network events"),
+    perfetto::Category("audio.latency")
+        .SetTags("verbose")
+        .SetDescription("Detailed audio latency metrics"));
+
+#endif  // TRACE_CATEGORIES_H
