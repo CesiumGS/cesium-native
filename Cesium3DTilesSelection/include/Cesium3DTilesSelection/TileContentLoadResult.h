968285d5cb3cac19df4621c7e76a57b116707cf2
Nithin Pranesh <nithinpra7@gmail.com>
2021-11-16 07:36:00 -0500

    Update Cesium3DTilesSelection/include/Cesium3DTilesSelection/TileContentLoadResult.h
    Co-authored-by: Kevin Ring <kevin@kotachrome.com>

--- Cesium3DTilesSelection/include/Cesium3DTilesSelection/TileContentLoadResult.h
+++ Cesium3DTilesSelection/include/Cesium3DTilesSelection/TileContentLoadResult.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <CesiumGeometry/QuadtreeTileRectangularRange.h>
-#include "CesiumGeospatial/Projection.h"
+#include <CesiumGeospatial/Projection.h>
 #include "Tile.h"
 #include "TileContext.h"
 
