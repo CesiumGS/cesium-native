e9523aec604b013677139fdcd446bc6684d19dd6
Sean Lilley <lilleyse@gmail.com>
2021-12-03 15:14:21 -0500

    New CesiumGltfWriter

---
+++ CesiumGltfWriter/include/CesiumGltfWriter/GltfWriter.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "CesiumGltfWriter/Library.h"
+
+#include <CesiumJsonWriter/ExtensionWriterContext.h>
+
+// forward declarations
+namespace CesiumGltf {
+struct Model;
+}
+
+namespace CesiumGltfWriter {
+
+/**
+ * @brief The result of writing a glTF with
+ * {@link GltfWriter::writeGltf} or {@link GltfWriter::writeGlb}
+ */
+struct CESIUMGLTFWRITER_API GltfWriterResult {
+  /**
+   * @brief The final generated std::vector<std::byte> of the glTF or glb.
+   */
+  std::vector<std::byte> gltfBytes;
+
+  /**
+   * @brief Errors, if any, that occurred during the write process.
+   */
+  std::vector<std::string> errors;
+
+  /**
+   * @brief Warnings, if any, that occurred during the write process.
+   */
+  std::vector<std::string> warnings;
+};
+
+/**
+ * @brief Writes glTF.
+ */
+class CESIUMGLTFWRITER_API GltfWriter {
+public:
+  /**
+   * @brief Constructs a new instance.
+   */
+  GltfWriter();
+
+  /**
+   * @brief Gets the context used to control how glTF extensions are written.
+   */
+  CesiumJsonWriter::ExtensionWriterContext& getExtensions();
+
+  /**
+   * @brief Gets the context used to control how glTF extensions are written.
+   */
+  const CesiumJsonWriter::ExtensionWriterContext& getExtensions() const;
+
+  /**
+   * @brief Serializes the provided model into a glTF JSON byte vector.
+   *
+   * @details Ignores internal data such as {@link CesiumGltf::BufferCesium}
+   * and {@link CesiumGltf::ImageCesium} when serializing the glTF. Internal
+   * data must either be converted to data uris or saved as external files. The
+   * buffer.uri and image.uri fields must be set accordingly prior to calling
+   * this function.
+   *
+   * @param model The model.
+   * @return The result of writing the glTF.
+   */
+  GltfWriterResult writeGltf(const CesiumGltf::Model& model) const;
+
+  /**
+   * @brief Serializes the provided model into a glb byte vector.
+   *
+   * @details The first buffer object implicitly refers to the GLB binary chunk
+   * and should not have a uri. Ignores internal data such as
+   * {@link CesiumGltf::BufferCesium} and {@link CesiumGltf::ImageCesium}.
+   *
+   * @param model The model.
+   * @param bufferData The buffer data to store in the GLB binary chunk.
+   * @return The result of writing the glb.
+   */
+  GltfWriterResult writeGlb(
+      const CesiumGltf::Model& model,
+      const std::vector<std::byte>& bufferData) const;
+
+private:
+  CesiumJsonWriter::ExtensionWriterContext _context;
+};
+
+} // namespace CesiumGltfWriter
---
+++ CesiumGltfWriter/src/GltfWriter.cpp
@@ -0,0 +1,124 @@
+#include "CesiumGltfWriter/GltfWriter.h"
+
+#include "ModelJsonWriter.h"
+
+#include <CesiumJsonWriter/JsonWriter.h>
+#include <CesiumJsonWriter/PrettyJsonWriter.h>
+#include <CesiumUtility/Tracing.h>
+
+namespace CesiumGltfWriter {
+
+namespace {
+
+[[nodiscard]] size_t
+getPadding(size_t byteLength, size_t byteOffset = 0) noexcept {
+  size_t boundary = 8;
+  size_t remainder = (byteOffset + byteLength) % boundary;
+  size_t padding = remainder == 0 ? 0 : boundary - remainder;
+  return padding;
+}
+
+[[nodiscard]] std::vector<std::byte> writeGlbBuffer(
+    const std::vector<std::byte>& jsonBuffer,
+    const std::vector<std::byte>& binaryBuffer) {
+
+  size_t jsonPaddingSize = getPadding(jsonBuffer.size(), 20);
+  size_t jsonChunkSize = jsonBuffer.size() + jsonPaddingSize;
+  size_t binaryPaddingSize = getPadding(binaryBuffer.size());
+  size_t binaryChunkSize = binaryBuffer.size() + binaryPaddingSize;
+  size_t glbSize = 12 + 8 + jsonChunkSize + 8 + binaryChunkSize;
+
+  std::vector<std::byte> glb(glbSize);
+  uint8_t* glb8 = reinterpret_cast<uint8_t*>(glb.data());
+  uint32_t* glb32 = reinterpret_cast<uint32_t*>(glb.data());
+
+  // GLB header
+  size_t byteOffset = 0;
+  glb8[byteOffset++] = 'g';
+  glb8[byteOffset++] = 'l';
+  glb8[byteOffset++] = 'T';
+  glb8[byteOffset++] = 'F';
+  glb32[byteOffset / 4] = 2;
+  byteOffset += 4;
+  glb32[byteOffset / 4] = static_cast<uint32_t>(glbSize);
+  byteOffset += 4;
+
+  // JSON chunk header
+  glb32[byteOffset / 4] = static_cast<uint32_t>(jsonChunkSize);
+  byteOffset += 4;
+  glb8[byteOffset++] = 'J';
+  glb8[byteOffset++] = 'S';
+  glb8[byteOffset++] = 'O';
+  glb8[byteOffset++] = 'N';
+
+  // JSON chunk
+  memcpy(glb8 + byteOffset, jsonBuffer.data(), jsonBuffer.size());
+  byteOffset += jsonBuffer.size();
+
+  // JSON chunk padding
+  memset(glb8 + byteOffset, ' ', jsonPaddingSize);
+  byteOffset += jsonPaddingSize;
+
+  // Binary chunk header
+  glb32[byteOffset / 4] = static_cast<uint32_t>(binaryChunkSize);
+  byteOffset += 4;
+  glb8[byteOffset++] = 'B';
+  glb8[byteOffset++] = 'I';
+  glb8[byteOffset++] = 'N';
+  glb8[byteOffset++] = 0;
+
+  // Binary chunk
+  memcpy(glb8 + byteOffset, binaryBuffer.data(), binaryBuffer.size());
+  byteOffset += binaryBuffer.size();
+
+  // Binary chunk padding
+  memset(glb8 + byteOffset, 0, binaryPaddingSize);
+
+  return glb;
+}
+} // namespace
+
+GltfWriter::GltfWriter() { registerExtensions(this->_context); }
+
+CesiumJsonWriter::ExtensionWriterContext& GltfWriter::getExtensions() {
+  return this->_context;
+}
+
+const CesiumJsonWriter::ExtensionWriterContext&
+GltfWriter::getExtensions() const {
+  return this->_context;
+}
+
+GltfWriterResult GltfWriter::writeGltf(const CesiumGltf::Model& model) const {
+  CESIUM_TRACE("GltfWriter::writeGltf");
+
+  const CesiumJsonWriter::ExtensionWriterContext& context =
+      this->getExtensions();
+
+  GltfWriterResult result;
+  CesiumJsonWriter::PrettyJsonWriter writer;
+
+  ModelJsonWriter::write(model, writer, context);
+  result.gltfBytes = writer.toBytes();
+
+  return result;
+}
+
+GltfWriterResult GltfWriter::writeGlb(
+    const CesiumGltf::Model& model,
+    const std::vector<std::byte>& bufferData) const {
+  CESIUM_TRACE("GltfWriter::writeGlb");
+
+  const CesiumJsonWriter::ExtensionWriterContext& context =
+      this->getExtensions();
+
+  GltfWriterResult result;
+  CesiumJsonWriter::JsonWriter writer;
+
+  ModelJsonWriter::write(model, writer, context);
+  result.gltfBytes = writeGlbBuffer(writer.toBytes(), bufferData);
+
+  return result;
+}
+
+} // namespace CesiumGltfWriter
