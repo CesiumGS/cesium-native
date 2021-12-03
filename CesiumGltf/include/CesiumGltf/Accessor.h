33a4768e739c0fe81debe833ecec8c0e2c82962d
Sean Lilley <lilleyse@gmail.com>
2021-12-02 19:21:53 -0500

    Revert "Revert changes in Accessor"
    This reverts commit 8ab739985e12d246bbf9fdb1f22d5ed152973bbc.

--- CesiumGltf/include/CesiumGltf/Accessor.h
+++ CesiumGltf/include/CesiumGltf/Accessor.h
@@ -32,7 +32,48 @@ struct CESIUMGLTF_API Accessor final : public AccessorSpec {
    * @return The number of bytes for the component type. Returns 0 if
    * {@link Accessor::componentType} is not a valid enumeration value.
    */
-  static int8_t computeByteSizeOfComponent(int32_t componentType) noexcept;
+  static int8_t
+  computeByteSizeOfComponent(const int32_t componentType) noexcept;
+
+  /**
+   * @brief Computes the number of bytes in each element for a given accessor
+   * type and accessor component type.
+   *
+   * This is computed by multiplying
+   * {@link Accessor::computeByteSizeOfComponent} by
+   * {@link Accessor::computeNumberOfComponents}.
+   *
+   * @param type The accessor type.
+   * @param componentType The accessor component type.
+   * @return The number of bytes in each element. Returns 0 if
+   * {@link Accessor::type} or {@link Accessor::componentType}
+   * does not have a valid enumeration value.
+   */
+  static int64_t computeByteSizeOfElement(
+      const std::string& type,
+      const int32_t componentType) noexcept;
+
+  /**
+   * @brief Computes the number of bytes in the accessor for a given accessor
+   * type, accessor component type, and accessor count.
+   *
+   * This is computed by multiplying
+   * {@link Accessor::computeByteSizeOfElement} by
+   * {@link Accessor::count}.
+   *
+   * @param type The accessor type.
+   * @param componentType The accessor component type.
+   * @param count The accessor count.
+   * @return The number of bytes in the accessor. Returns 0 if
+   * {@link Accessor::type} or {@link Accessor::componentType} does not have a
+   * valid enumeration value.
+   */
+  static int64_t computeByteSize(
+      const std::string& type,
+      const int32_t componentType,
+      const int64_t count) noexcept;
+
+  static int32_t computeIndexComponentType(const int64_t vertexCount);
 
   /**
    * @brief Computes the number of components for this accessor.
@@ -61,25 +102,37 @@ struct CESIUMGLTF_API Accessor final : public AccessorSpec {
   int8_t computeByteSizeOfComponent() const noexcept;
 
   /**
-   * @brief Computes the total number of bytes for this accessor in each vertex.
+   * @brief Computes the number of bytes for each element in this accessor.
    *
    * This is computed by multiplying
    * {@link Accessor::computeByteSizeOfComponent} by
    * {@link Accessor::computeNumberOfComponents}.
    *
-   * @return The total number of bytes for this accessor in each vertex. Returns
-   * 0 if this accessor's {@link Accessor::type} or
-   * {@link Accessor::componentType} does not have a valid enumeration
-   * value.
+   * @return The total number of bytes for each element. Returns 0 if this
+   * accessor's {@link Accessor::type} or {@link Accessor::componentType} does
+   * not have a valid enumeration value.
    */
-  int64_t computeBytesPerVertex() const noexcept;
+  int64_t computeByteSizeOfElement() const noexcept;
+
+  /**
+   * @brief Computes the total number of bytes for this accessor.
+   *
+   * This is computed by multiplying
+   * {@link Accessor::computeByteSizeOfElement} by
+   * {@link Accessor::count}.
+   *
+   * @return The total number of bytes for this accessor. Returns 0 if this
+   * accessor's {@link Accessor::type} or {@link Accessor::componentType} does
+   * not have a valid enumeration value.
+   */
+  int64_t computeByteSize() const noexcept;
 
   /**
    * @brief Computes this accessor's stride.
    *
    * The stride is the number of bytes between the same elements of successive
    * vertices. The returned value will be at least as large as
-   * {@link Accessor::computeBytesPerVertex}, but maybe be larger if this
+   * {@link Accessor::computeByteSizeOfElement}, but maybe be larger if this
    * accessor's data is interleaved with other accessors.
    *
    * The behavior is undefined if this accessor is not part of the given model.
