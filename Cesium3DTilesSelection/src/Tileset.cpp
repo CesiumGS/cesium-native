b2ab24e2bfbcf864073f99d8ef26cf76c0ad2fd5
Joseph Kaile <joseph.kaile@simblocks.io>
2022-01-27 12:03:32 -0500

    fix case when 'REP' is accepted for 'REPLACE'

--- Cesium3DTilesSelection/src/Tileset.cpp
+++ Cesium3DTilesSelection/src/Tileset.cpp
@@ -1050,37 +1050,29 @@ static void parseImplicitTileset(
       tile.setRefine(TileRefine::Replace);
     } else if (refine == "ADD") {
       tile.setRefine(TileRefine::Add);
-    } else if (std::equal(
-                   refine.begin(),
-                   refine.end(),
-                   "REPLACE",
-                   [](char i, char j) {
-                     return (std::tolower(i) == std::tolower(j));
-                   })) {
-      tile.setRefine(TileRefine::Replace);
-      SPDLOG_LOGGER_ERROR(
-          pLogger,
-          "Tile refine value '{}' should be all uppercase: '{}'",
-          refine,
-          "REPLACE");
-    } else if (std::equal(
-                   refine.begin(),
-                   refine.end(),
-                   "ADD",
-                   [](char i, char j) {
-                     return (std::tolower(i) == std::tolower(j));
-                   })) {
-      tile.setRefine(TileRefine::Add);
-      SPDLOG_LOGGER_ERROR(
-          pLogger,
-          "Tile refine value '{}' should be all uppercase: '{}'",
-          refine,
-          "ADD");
     } else {
-      SPDLOG_LOGGER_ERROR(
-          pLogger,
-          "Tile contained an unknown refine value: {}",
-          refine);
+      std::string refineUpper = refine;
+      std::transform(
+          refineUpper.begin(),
+          refineUpper.end(),
+          refineUpper.begin(),
+          [](unsigned char c) -> char {
+            return static_cast<unsigned char>(std::toupper(c));
+          });
+      if (refineUpper == "REPLACE" || refineUpper == "ADD") {
+        SPDLOG_LOGGER_ERROR(
+            pLogger,
+            "Tile refine value '{}' should be uppercase: '{}'",
+            refine,
+            refineUpper);
+        tile.setRefine(
+            refineUpper == "REPLACE" ? TileRefine::Replace : TileRefine::Add);
+      } else {
+        SPDLOG_LOGGER_ERROR(
+            pLogger,
+            "Tile contained an unknown refine value: {}",
+            refine);
+      }
     }
   } else {
     tile.setRefine(parentRefine);
