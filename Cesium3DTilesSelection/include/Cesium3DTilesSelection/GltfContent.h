39c7208b14af3d759ba6a0385361ca3cd7c04c74
Joseph Kaile <joseph.kaile@simblocks.io>
2022-02-01 15:30:38 -0500

    update comments

--- Cesium3DTilesSelection/include/Cesium3DTilesSelection/GltfContent.h
+++ Cesium3DTilesSelection/include/Cesium3DTilesSelection/GltfContent.h
@@ -137,11 +137,11 @@ public:
   /**
    * @brief Applies the glTF's RTC_CENTER, if any, to the given transform.
    *
-   * If the glTF has a 3-element numeric array under the name `RTC_CENTER`, this
-   * function will multiply the given matrix with the (translation) matrix that
-   * is created from this `RTC_CENTER` property in the `extras` of the given
-   * model. If the given model does not have this property, then this function
-   * will return the `rootTransform` unchanged.
+   * If the glTF has a Cesium RTC extension, this function will multiply the
+   * given matrix with the (translation) matrix that is created from the
+   * `RTC_CENTER` in the Cesium RTC extension of the given model. If the given
+   * model does not have this property, then this function will return the
+   * `rootTransform` unchanged.
    *
    * @param model The glTF model
    * @param rootTransform The matrix that will be multiplied with the transform
