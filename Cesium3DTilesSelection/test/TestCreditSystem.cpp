8364bc8c5a9282ae3ceeab9d60dbabe10082cb2c
Joseph Kaile <joseph.kaile@simblocks.io>
2022-03-07 21:09:28 -0500

    check if expected == actual using std::permutation

--- Cesium3DTilesSelection/test/TestCreditSystem.cpp
+++ Cesium3DTilesSelection/test/TestCreditSystem.cpp
@@ -26,7 +26,10 @@ TEST_CASE("Test basic credit handling") {
   REQUIRE(creditSystem.getCreditsToShowThisFrame() == expectedShow0);
 
   std::vector<Credit> expectedHide0{};
-  REQUIRE(creditSystem.getCreditsToNoLongerShowThisFrame() == expectedHide0);
+  REQUIRE(std::is_permutation(
+      expectedShow0.begin(),
+      expectedShow0.end(),
+      creditSystem.getCreditsToShowThisFrame().begin()));
 
   // Start frame 1: Add 1 and 2, remove 0
   creditSystem.startNextFrame();
@@ -35,7 +38,10 @@ TEST_CASE("Test basic credit handling") {
   creditSystem.addCreditToFrame(credit2);
 
   std::vector<Credit> expectedShow1{credit2, credit1};
-  REQUIRE(creditSystem.getCreditsToShowThisFrame() == expectedShow1);
+  REQUIRE(std::is_permutation(
+      expectedShow1.begin(),
+      expectedShow1.end(),
+      creditSystem.getCreditsToShowThisFrame().begin()));
 
   std::vector<Credit> expectedHide1{credit0};
   REQUIRE(creditSystem.getCreditsToNoLongerShowThisFrame() == expectedHide1);
@@ -44,7 +50,10 @@ TEST_CASE("Test basic credit handling") {
   creditSystem.startNextFrame();
 
   std::vector<Credit> expectedShow2{};
-  REQUIRE(creditSystem.getCreditsToShowThisFrame() == expectedShow2);
+  REQUIRE(std::is_permutation(
+      expectedShow2.begin(),
+      expectedShow2.end(),
+      creditSystem.getCreditsToShowThisFrame().begin()));
 
   std::vector<Credit> expectedHide2{credit2, credit1};
   REQUIRE(creditSystem.getCreditsToNoLongerShowThisFrame() == expectedHide2);
