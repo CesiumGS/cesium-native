bdc620a84edff09ff4197283e97118165c30b4a7
Bao Tran <tranthienbao123@gmail.com>
2022-06-22 16:50:59 -0400

    update sampler

--- Cesium3DTilesSelection/src/RasterOverlayUpsampler.cpp
+++ Cesium3DTilesSelection/src/RasterOverlayUpsampler.cpp
@@ -1,3 +1,18 @@
 #include "RasterOverlayUpsampler.h"
 
-namespace Cesium3DTilesSelection {}
+namespace Cesium3DTilesSelection {
+CesiumAsync::Future<TileLoadResult> RasterOverlayUpsampler::loadTileContent(
+    Tile& tile,
+    const TilesetContentOptions& contentOptions,
+    const CesiumAsync::AsyncSystem& asyncSystem,
+    const std::shared_ptr<CesiumAsync::IAssetAccessor>& pAssetAccessor,
+    const std::shared_ptr<spdlog::logger>& pLogger,
+    const std::vector<CesiumAsync::IAssetAccessor::THeader>& requestHeaders)
+{
+
+}
+
+bool RasterOverlayUpsampler::updateTileContent([[maybe_unused]] Tile& tile) {
+  return false;
+}
+} // namespace Cesium3DTilesSelection
