315579c2f44bab3939096bd234f2b23a3b826cb6
Bao Tran <tranthienbao123@gmail.com>
2022-08-02 14:18:35 -0400

    test state machine of upsampled children

--- Cesium3DTilesSelection/test/TestTilesetContentManager.cpp
+++ Cesium3DTilesSelection/test/TestTilesetContentManager.cpp
@@ -9,10 +9,12 @@
 #include <Cesium3DTilesSelection/Tile.h>
 #include <Cesium3DTilesSelection/TilesetContentLoader.h>
 #include <Cesium3DTilesSelection/registerAllTileContentTypes.h>
+#include <CesiumGeometry/QuadtreeTileID.h>
 
 #include <catch2/catch.hpp>
 
 using namespace Cesium3DTilesSelection;
+using namespace CesiumGeometry;
 
 namespace {
 class SimpleTilesetContentLoader : public TilesetContentLoader {
@@ -301,5 +303,118 @@ TEST_CASE("Test tile state machine") {
     CHECK(!tile.getContent().isEmptyContent());
     CHECK(!tile.getContent().getTileInitializerCallback());
     CHECK(!tile.getContent().getRenderResources());
+  }
+
+  SECTION("Make sure the manager loads parent first before loading upsampled "
+          "child") {
+    // create mock loader
+    bool initializerCall = false;
+    auto pMockedLoader = std::make_unique<SimpleTilesetContentLoader>();
+    auto pMockedLoaderRaw = pMockedLoader.get();
+
+    pMockedLoader->mockLoadTileContent = {
+        TileRenderContent{CesiumGltf::Model()},
+        std::nullopt,
+        std::nullopt,
+        std::nullopt,
+        nullptr,
+        [&](Tile&) { initializerCall = true; },
+        TileLoadResultState::Success};
+    pMockedLoader->mockCreateTileChildren = {{}, TileLoadResultState::Failed};
+
+    // create manager
+    TilesetContentManager manager{
+        externals,
+        {},
+        std::move(pMockedLoader),
+        rasterOverlayCollection};
+
+    // create tile
+    Tile tile(pMockedLoaderRaw);
+    tile.setTileID(QuadtreeTileID(0, 0, 0));
+
+    // create upsampled children. We put it in the scope since upsampledTile
+    // will be moved to the parent afterward, and we don't let the tests below
+    // to use it accidentally
+    {
+      std::vector<Tile> children;
+      children.emplace_back(pMockedLoaderRaw);
+      Tile& upsampledTile = children.back();
+      upsampledTile.setTileID(UpsampledQuadtreeNode{QuadtreeTileID(1, 1, 1)});
+      tile.createChildTiles(std::move(children));
+    }
+
+    Tile& upsampledTile = tile.getChildren().back();
+
+    // test manager loading upsample tile
+    TilesetOptions options{};
+    options.contentOptions.generateMissingNormalsSmooth = true;
+    manager.loadTileContent(upsampledTile, options);
+
+    // since parent is not yet loaded, it will load the parent first.
+    // The upsampled tile will not be loaded at the moment
+    CHECK(upsampledTile.getState() == TileLoadState::Unloaded);
+    CHECK(tile.getState() == TileLoadState::ContentLoading);
+
+    // parent moves from ContentLoading -> ContentLoaded
+    asyncSystem.dispatchMainThreadTasks();
+    CHECK(tile.getState() == TileLoadState::ContentLoaded);
+    CHECK(tile.isRenderContent());
+    CHECK(tile.getContent().getTileInitializerCallback());
+    CHECK(!initializerCall);
+
+    // try again with upsample tile, but still not able to load it
+    // because parent is not done yet
+    manager.loadTileContent(upsampledTile, options);
+    CHECK(upsampledTile.getState() == TileLoadState::Unloaded);
+
+    // parent moves from ContentLoaded -> Done
+    manager.updateTileContent(tile, options);
+    CHECK(tile.getState() == TileLoadState::Done);
+    CHECK(tile.getChildren().size() == 1);
+    CHECK(&tile.getChildren().back() == &upsampledTile);
+    CHECK(tile.isRenderContent());
+    CHECK(!tile.getContent().getTileInitializerCallback());
+    CHECK(initializerCall);
+
+    // load the upsampled tile again: Unloaded -> ContentLoading
+    initializerCall = false;
+    pMockedLoaderRaw->mockLoadTileContent = {
+        TileRenderContent{CesiumGltf::Model()},
+        std::nullopt,
+        std::nullopt,
+        std::nullopt,
+        nullptr,
+        [&](Tile&) { initializerCall = true; },
+        TileLoadResultState::Success};
+    pMockedLoaderRaw->mockCreateTileChildren = {
+        {},
+        TileLoadResultState::Failed};
+    manager.loadTileContent(upsampledTile, options);
+    CHECK(upsampledTile.getState() == TileLoadState::ContentLoading);
+
+    // trying to unload parent while upsampled children is loading won't work
+    CHECK(!manager.unloadTileContent(tile));
+    CHECK(tile.getState() == TileLoadState::Done);
+    CHECK(tile.isRenderContent());
+
+    // upsampled tile: ContentLoading -> ContentLoaded
+    asyncSystem.dispatchMainThreadTasks();
+    CHECK(upsampledTile.getState() == TileLoadState::ContentLoaded);
+    CHECK(upsampledTile.isRenderContent());
+    CHECK(upsampledTile.getContent().getTileInitializerCallback());
+
+    // trying to unload parent will work now since the upsampled tile is already
+    // in the main thread
+    CHECK(manager.unloadTileContent(tile));
+    CHECK(tile.getState() == TileLoadState::Unloaded);
+    CHECK(!tile.isRenderContent());
+    CHECK(!tile.getContent().getRenderResources());
+
+    // trying to unload upsampled tile
+    CHECK(manager.unloadTileContent(upsampledTile));
+    CHECK(upsampledTile.getState() == TileLoadState::Unloaded);
+    CHECK(!upsampledTile.isRenderContent());
+    CHECK(!upsampledTile.getContent().getRenderResources());
   }
 }
