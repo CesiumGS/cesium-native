60ca8dc13d28d2b30c4f89ad7fae9df44dd7ccd6
Joseph Kaile <joseph.kaile@simblocks.io>
2022-08-04 17:19:55 -0400

    add test to check that gltf is no throw move constructable

--- CesiumGltfReader/test/TestGltfReader.cpp
+++ CesiumGltfReader/test/TestGltfReader.cpp
@@ -12,6 +12,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <type_traits>
 
 using namespace CesiumGltf;
 using namespace CesiumGltfReader;
@@ -31,6 +32,10 @@ std::vector<std::byte> readFile(const std::filesystem::path& fileName) {
   return buffer;
 }
 } // namespace
+
+TEST_CASE("CesiumGltf::Model move constructor is noexcept") {
+  CHECK(std::is_nothrow_move_constructible<CesiumGltf::Model>::value);
+}
 
 TEST_CASE("CesiumGltfReader::GltfReader") {
   using namespace std::string_literals;
