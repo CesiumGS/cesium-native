3557c35c5b75f1400f2d4733db2dd80a1406009f
Alex Gallegos <39537389+argallegos@users.noreply.github.com>
2023-02-06 11:47:47 -0500

    add temp north offset

--- CesiumGeospatial/src/SunPosition.cpp
+++ CesiumGeospatial/src/SunPosition.cpp
@@ -3,6 +3,10 @@
 namespace CesiumGeospatial {
 
 	/*static*/ glm::dvec3 SunPosition::getSunAngle(float time) {
-		return glm::dvec3(((time * 15.0) - 90.0f), 0.0, 0.0);
+
+		// North offset is a temporary approximation to align the sun position east to west.
+		float northOffset = -90.0;
+
+		return glm::dvec3(((time * 15.0) - 90.0f), northOffset, 0.0);
 	 }
 }
