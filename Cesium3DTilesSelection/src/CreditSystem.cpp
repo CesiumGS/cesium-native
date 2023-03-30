374d1da69429da2d76b6cc4d793626d0912754f5
Janine Liu <janine.h.liu@gmail.com>
2023-03-30 17:25:36 -0400

    Add ability to override showOnScreen in credit

--- Cesium3DTilesSelection/src/CreditSystem.cpp
+++ Cesium3DTilesSelection/src/CreditSystem.cpp
@@ -8,14 +8,14 @@ Credit CreditSystem::createCredit(const std::string& html, bool showOnScreen) {
   // if this credit already exists, return a Credit handle to it
   for (size_t id = 0; id < _credits.size(); ++id) {
     if (_credits[id].html == html) {
+      // Override the existing credit's showOnScreen value.
+      _credits[id].showOnScreen = showOnScreen;
       return Credit(id);
     }
   }
 
-  // this is a new credit so add it to _credits
   _credits.push_back({html, showOnScreen, -1, 0});
 
-  // return a Credit handle to the newly created entry
   return Credit(_credits.size() - 1);
 }
 
