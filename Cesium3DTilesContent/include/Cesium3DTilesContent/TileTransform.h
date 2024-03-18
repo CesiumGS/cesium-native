4be1cbb758c99219cbe96d20c87535ed09f10314
Kevin Ring <kevin@kotachrome.com>
2024-03-18 14:17:23 +1100

    Add TileTransform::setTransform.

--- Cesium3DTilesContent/include/Cesium3DTilesContent/TileTransform.h
+++ Cesium3DTilesContent/include/Cesium3DTilesContent/TileTransform.h
@@ -10,10 +10,30 @@ struct Tile;
 
 namespace Cesium3DTilesContent {
 
+/**
+ * @brief Convenience functions for getting and setting
+ * {@link Cesium3DTiles::Tile::transform} as a `glm::dmat4`.
+ */
 class TileTransform {
 public:
+  /**
+   * @brief Gets the tile's transform as a `glm::dmat4`.
+   *
+   * @param tile The tile from which to get the transform.
+   * @return The transform, or `std::nullopt` if the
+   * {@link Cesium3DTiles::Tile::transform} does not have 16 elements.
+   */
   static std::optional<glm::dmat4>
   getTransform(const Cesium3DTiles::Tile& tile);
+
+  /**
+   * @brief Sets the tile's transform as a `glm::dmat4`.
+   *
+   * @param tile The tile on which to set the transform.
+   * @param newTransform The new transform.
+   */
+  static void
+  setTransform(Cesium3DTiles::Tile& tile, const glm::dmat4& newTransform);
 };
 
 } // namespace Cesium3DTilesContent
