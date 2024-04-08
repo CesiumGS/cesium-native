b89f43db423f5eda469cbb29d9f1eb21271e131a
Kevin Ring <kevin@kotachrome.com>
2024-04-08 19:31:38 +1000

    Add test for CESIUM_RTC in extensionsUsed.

---
+++ Cesium3DTilesContent/test/TestB3dmToGltfConverter.cpp
@@ -0,0 +1,23 @@
+#include "ConvertTileToGltf.h"
+
+#include <CesiumGltf/ExtensionCesiumRTC.h>
+
+#include <catch2/catch.hpp>
+
+using namespace Cesium3DTilesContent;
+using namespace CesiumGltf;
+
+TEST_CASE("B3dmToGltfConverter") {
+  SECTION("includes CESIUM_RTC extension in extensionsUsed") {
+    std::filesystem::path testFilePath = Cesium3DTilesSelection_TEST_DATA_DIR;
+    testFilePath = testFilePath / "BatchTables" / "batchedWithJson.b3dm";
+
+    GltfConverterResult result = ConvertTileToGltf::fromB3dm(testFilePath);
+
+    REQUIRE(result.model);
+
+    Model& gltf = *result.model;
+    REQUIRE(gltf.getExtension<ExtensionCesiumRTC>() != nullptr);
+    CHECK(gltf.isExtensionUsed(ExtensionCesiumRTC::ExtensionName));
+  }
+}
