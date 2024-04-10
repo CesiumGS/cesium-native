40f16b1722a48b349ac5f30d6bee0da675219f90
Janine Liu <janine@cesium.com>
2024-04-10 11:01:57 -0400

    Add GlobeRectangle equals and equalsEpsilon

--- CesiumGeospatial/include/CesiumGeospatial/GlobeRectangle.h
+++ CesiumGeospatial/include/CesiumGeospatial/GlobeRectangle.h
@@ -241,6 +241,30 @@ public:
    */
   GlobeRectangle computeUnion(const GlobeRectangle& other) const noexcept;
 
+  /**
+   * @brief Checks whether two globe rectangles are exactly equal.
+   *
+   * @param left The first rectangle.
+   * @param right The second rectangle.
+   * @return Whether the rectangles are equal
+   */
+  static bool
+  equals(const GlobeRectangle& left, const GlobeRectangle& right) noexcept;
+
+  /**
+   * @brief Checks whether two globe rectangles are equal up to a given relative
+   * epsilon.
+   *
+   * @param left The first rectangle.
+   * @param right The second rectangle.
+   * @param relativeEpsilon The relative epsilon.
+   * @return Whether the rectangles are epsilon-equal
+   */
+  static bool equalsEpsilon(
+      const GlobeRectangle& left,
+      const GlobeRectangle& right,
+      double relativeEpsilon) noexcept;
+
 private:
   double _west;
   double _south;
