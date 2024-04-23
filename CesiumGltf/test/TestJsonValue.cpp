8b29e75b7f4e60666475e50380b7608421579ae5
Brian L <130494071+csciguy8@users.noreply.github.com>
2024-04-23 16:03:54 -0600

    Revert "Remove test. This passes for intel, fails for arm64 (macos)"
    This reverts commit 9463df2796e5937ed9412647e69f876f57666ca3.

--- CesiumGltf/test/TestJsonValue.cpp
+++ CesiumGltf/test/TestJsonValue.cpp
@@ -41,6 +41,11 @@ TEST_CASE(
 
 TEST_CASE("JsonValue::getSafeNumber() throws if narrowing conversion error "
           "would occur") {
+  SECTION("2^64 - 1 cannot be converted back to a double") {
+    auto value = JsonValue(std::numeric_limits<std::uint64_t>::max());
+    REQUIRE_THROWS(value.getSafeNumber<double>());
+  }
+
   SECTION("-2^64 cannot be converted back to a double") {
     // -9223372036854775807L cannot be represented exactly as a double
     auto value = JsonValue(std::numeric_limits<std::int64_t>::min() + 1);
