9661e40ead745074030e4eccc72fb7882e700664
Tim Moore <tim.moore@cesium.com>
2024-04-26 11:19:28 +0200

    Refactor and support existing instances in GLB
    Simplified the code by not trying to optimize the inclusion of
    rotation and scale transforms.

--- Cesium3DTilesContent/src/I3dmToGltfConverter.cpp
+++ Cesium3DTilesContent/src/I3dmToGltfConverter.cpp
@@ -4,8 +4,11 @@
 #include <Cesium3DTilesContent/I3dmToGltfConverter.h>
 #include <Cesium3DTilesContent/LegacyUtilities.h>
 #include <CesiumGeospatial/LocalHorizontalCoordinateSystem.h>
+#include <CesiumGltf/AccessorUtility.h>
+#include <CesiumGltf/AccessorView.h>
 #include <CesiumGltf/ExtensionExtMeshGpuInstancing.h>
 #include <CesiumGltf/Model.h>
+#include <CesiumGltf/PropertyTransformations.h>
 #include <CesiumGltfContent/GltfUtilities.h>
 #include <CesiumUtility/AttributeCompression.h>
 
@@ -457,27 +460,183 @@ CesiumAsync::Future<ConvertResult> convertInstancesContent(
   }
 }
 
-// XXX If there are no scale or rotation parts to the instance transform, then
-// there shouldn't be any after applying the forward and inverse glTF
-// transforms. Should prove this!
+glm::dmat4
+composeInstanceTransform(size_t i, const DecodedInstances& decodedInstances) {
+  glm::dmat4 result(1.0);
+  if (!decodedInstances.positions.empty()) {
+    result = translate(result, glm::dvec3(decodedInstances.positions[i]));
+  }
+  if (!decodedInstances.rotations.empty()) {
+    result = result * toMat4(glm::dquat(decodedInstances.rotations[i]));
+  }
+  if (!decodedInstances.scales.empty()) {
+    result = scale(result, glm::dvec3(decodedInstances.scales[i]));
+  }
+  return result;
+}
+
+// Helpers for different instancing rotation formats
+
+template <typename T> struct is_float_quat : std::false_type {};
+
+template <>
+struct is_float_quat<CesiumGltf::AccessorTypes::VEC4<float>> : std::true_type {
+};
+
+template <typename T> struct is_int_quat : std::false_type {};
+
+template <typename T>
+struct is_int_quat<CesiumGltf::AccessorTypes::VEC4<T>>
+    : std::conjunction<std::is_integral<T>, std::is_signed<T>> {};
+
+template <typename T>
+inline constexpr bool is_float_quat_v = is_float_quat<T>::value;
+
+template <typename T>
+inline constexpr bool is_int_quat_v = is_int_quat<T>::value;
+
+std::vector<glm::dmat4> meshGpuInstances(
+    GltfConverterResult& result,
+    const ExtensionExtMeshGpuInstancing& gpuExt) {
+  const Model& model = *result.model;
+  std::vector<glm::dmat4> instances;
+  if (gpuExt.attributes.empty()) {
+    return instances;
+  }
+  auto getInstanceAccessor = [&](const char* name) -> const Accessor* {
+    if (auto accessorItr = gpuExt.attributes.find(name);
+        accessorItr != gpuExt.attributes.end()) {
+      return Model::getSafe(&model.accessors, accessorItr->second);
+    }
+    return nullptr;
+  };
+  auto errorOut = [&](const std::string& errorMsg) {
+    result.errors.emplaceError(errorMsg);
+    return instances;
+  };
+
+  const Accessor* translations = getInstanceAccessor("TRANSLATION");
+  const Accessor* rotations = getInstanceAccessor("ROTATION");
+  const Accessor* scales = getInstanceAccessor("SCALE");
+  int64_t count = 0;
+  if (translations) {
+    count = translations->count;
+  }
+  if (rotations) {
+    if (count == 0) {
+      count = rotations->count;
+    } else if (count != rotations->count) {
+      return errorOut(fmt::format(
+          "instance rotation count {} not consistent with {}",
+          rotations->count,
+          count));
+    }
+  }
+  if (scales) {
+    if (count == 0) {
+      count = scales->count;
+    } else if (count != scales->count) {
+      return errorOut(fmt::format(
+          "instance scale count {} not consistent with {}",
+          scales->count,
+          count));
+    }
+  }
+  if (count == 0) {
+    return errorOut("No valid instance data");
+  }
+  instances.resize(static_cast<size_t>(count), glm::dmat4(1.0));
+  if (translations) {
+    AccessorView<CesiumGltf::AccessorTypes::VEC3<float>> translationAccessor(
+        model,
+        *translations);
+    if (translationAccessor.status() == AccessorViewStatus::Valid) {
+      for (unsigned i = 0; i < count; ++i) {
+        glm::dvec3 transVec(
+            translationAccessor[i].value[0],
+            translationAccessor[i].value[1],
+            translationAccessor[i].value[2]);
+        instances[i] = glm::translate(instances[i], transVec);
+      }
+    } else {
+      return errorOut("invalid accessor for instance translations");
+    }
+  }
+  if (rotations) {
+    createAccessorView(model, *rotations, [&](auto&& quatView) -> void {
+      using QuatType = decltype(quatView[0]);
+      using ValueType = std::decay_t<QuatType>;
+      if constexpr (is_float_quat_v<ValueType>) {
+        for (unsigned i = 0; i < count; ++i) {
+          glm::dquat quat(
+              quatView[i].value[3],
+              quatView[i].value[0],
+              quatView[i].value[1],
+              quatView[i].value[2]);
+          instances[i] = instances[i] * glm::toMat4(quat);
+        }
+      } else if constexpr (is_int_quat_v<ValueType>) {
+        for (unsigned i = 0; i < count; ++i) {
+          float val[4];
+          for (unsigned j = 0; j < 4; ++j) {
+            val[j] = static_cast<float>(normalize(quatView[i].value[j]));
+          }
+          glm::dquat quat(val[3], val[0], val[1], val[2]);
+          instances[i] = instances[i] * glm::toMat4(quat);
+        }
+      }
+    });
+  }
+  if (scales) {
+    AccessorView<CesiumGltf::AccessorTypes::VEC3<float>> scaleAccessor(
+        model,
+        *scales);
+    if (scaleAccessor.status() == AccessorViewStatus::Valid) {
+      for (unsigned i = 0; i < count; ++i) {
+        glm::dvec3 scaleFactors(
+            scaleAccessor[i].value[0],
+            scaleAccessor[i].value[1],
+            scaleAccessor[i].value[2]);
+        instances[i] = glm::scale(instances[i], scaleFactors);
+      }
+    } else {
+      return errorOut("invalid accessor for instance translations");
+    }
+  }
+  return instances;
+}
+
+const size_t rotOffset = sizeof(glm::vec3);
+const size_t scaleOffset = rotOffset + sizeof(glm::quat);
+const size_t totalStride =
+    sizeof(glm::vec3) + sizeof(glm::quat) + sizeof(glm::vec3);
+
+void copyToBuffer(
+    const glm::dvec3& position,
+    const glm::dquat& rotation,
+    const glm::dvec3& scale,
+    std::byte* bufferLoc) {
+  glm::vec3 fposition(position);
+  std::memcpy(bufferLoc, &fposition, sizeof(fposition));
+  glm::quat frotation(rotation);
+  std::memcpy(bufferLoc + rotOffset, &frotation, sizeof(frotation));
+  glm::vec3 fscale(scale);
+  std::memcpy(bufferLoc + scaleOffset, &fscale, sizeof(fscale));
+}
+
+void copyToBuffer(
+    const glm::dvec3& position,
+    const glm::dquat& rotation,
+    const glm::dvec3& scale,
+    std::vector<std::byte>& bufferData,
+    size_t i) {
+  copyToBuffer(position, rotation, scale, &bufferData[i * totalStride]);
+}
 
 void instantiateInstances(
     GltfConverterResult& result,
     const DecodedInstances& decodedInstances) {
   std::set<CesiumGltf::Node*> meshNodes;
-  size_t totalStride = sizeof(float) * 3;
-  size_t rotOffset = 0;
-  const bool hasRotations = !decodedInstances.rotations.empty();
-  if (hasRotations) {
-    rotOffset = sizeof(float) * 3;
-    totalStride += sizeof(float) * 4;
-  }
-  size_t scaleOffset = 0;
-  const bool hasScales = !decodedInstances.scales.empty();
-  if (hasScales) {
-    scaleOffset = totalStride;
-    totalStride += sizeof(float) * 3;
-  }
   int32_t instanceBufferId = createBufferInGltf(*result.model);
   auto& instanceBuffer =
       result.model->buffers[static_cast<uint32_t>(instanceBufferId)];
@@ -490,7 +649,6 @@ void instantiateInstances(
       result.model->bufferViews[static_cast<uint32_t>(instanceBufferViewId)];
   const auto numInstances =
       static_cast<uint32_t>(decodedInstances.positions.size());
-  const size_t instanceDataSize = totalStride * numInstances;
   auto upToZ = CesiumGltfContent::GltfUtilities::applyGltfUpAxisTransform(
       *result.model,
       glm::dmat4x4(1.0));
@@ -505,87 +663,79 @@ void instantiateInstances(
         if (!notSeen) {
           return;
         }
+        std::vector<glm::dmat4> existingInstances{glm::dmat4(1.0)};
+        auto& gpuExt = node.addExtension<ExtensionExtMeshGpuInstancing>();
+        if (!gpuExt.attributes.empty()) {
+          // The model already has instances! We will need to create the outer
+          // product of these instances and those coming from i3dm.
+          existingInstances = meshGpuInstances(result, gpuExt);
+          if (numInstances * existingInstances.size() >
+              std::numeric_limits<uint32_t>::max()) {
+            result.errors.emplaceError(fmt::format(
+                "Too many instances: {} from i3dm and {} from glb",
+                numInstances,
+                existingInstances.size()));
+            return;
+          }
+        }
+        const uint32_t numNewInstances =
+            static_cast<uint32_t>(numInstances * existingInstances.size());
+        const size_t instanceDataSize = totalStride * numNewInstances;
         auto dataBaseOffset =
             static_cast<uint32_t>(instanceBuffer.cesium.data.size());
         instanceBuffer.cesium.data.resize(dataBaseOffset + instanceDataSize);
         // Transform instance transform into local glTF coordinate system.
         const auto toTile = upToZ * transform;
         const auto toTileInv = inverse(toTile);
+        size_t destInstanceIndx = 0;
         for (unsigned i = 0; i < numInstances; ++i) {
-          auto instMat = toTileInv;
-          instMat =
-              translate(instMat, glm::dvec3(decodedInstances.positions[i]));
-          if (hasRotations) {
-            instMat =
-                instMat * toMat4(glm::dquat(decodedInstances.rotations[i]));
+          auto instMat = composeInstanceTransform(i, decodedInstances);
+          instMat = toTileInv * instMat * toTile;
+          for (const auto& innerMat : existingInstances) {
+            auto finalMat = instMat * innerMat;
+            glm::dvec3 position, scale, skew;
+            glm::dquat rotation;
+            glm::dvec4 perspective;
+            decompose(finalMat, scale, rotation, position, skew, perspective);
+            copyToBuffer(
+                position,
+                rotation,
+                scale,
+                instanceBuffer.cesium.data,
+                destInstanceIndx++);
           }
-          if (hasScales) {
-            instMat = scale(instMat, glm::dvec3(decodedInstances.scales[i]));
-          }
-          instMat = instMat * toTile;
-          glm::dvec3 position, scale, skew;
-          glm::dquat rotation;
-          glm::dvec4 perspective;
-          decompose(instMat, scale, rotation, position, skew, perspective);
-          glm::vec3 fposition(position);
-          std::memcpy(
-              &instanceBuffer.cesium.data[i * totalStride],
-              &fposition,
-              sizeof(fposition));
-          if (hasRotations) {
-            glm::quat frotation(rotation);
-            std::memcpy(
-                &instanceBuffer.cesium.data[i * totalStride + rotOffset],
-                &frotation,
-                sizeof(frotation));
-          }
-          if (hasScales) {
-            glm::vec3 fscale(scale);
-            std::memcpy(
-                &instanceBuffer.cesium.data[i * totalStride + scaleOffset],
-                &fscale,
-                sizeof(fscale));
-          }
-        }
-        auto& gpuExt = node.addExtension<ExtensionExtMeshGpuInstancing>();
-        if (!gpuExt.attributes.empty()) {
-          // wtf
         }
         auto posAccessorId = createAccessorInGltf(
             gltf,
             instanceBufferViewId,
             Accessor::ComponentType::FLOAT,
-            numInstances,
+            numNewInstances,
             Accessor::Type::VEC3);
         auto& posAcessor = gltf.accessors[static_cast<uint32_t>(posAccessorId)];
         posAcessor.byteOffset = dataBaseOffset;
         gpuExt.attributes["TRANSLATION"] = posAccessorId;
-        if (hasRotations) {
-          auto rotAccessorId = createAccessorInGltf(
-              gltf,
-              instanceBufferViewId,
-              Accessor::ComponentType::FLOAT,
-              numInstances,
-              Accessor::Type::VEC4);
-          auto& rotAccessor =
-              gltf.accessors[static_cast<uint32_t>(rotAccessorId)];
-          rotAccessor.byteOffset =
-              static_cast<int64_t>(dataBaseOffset + rotOffset);
-          gpuExt.attributes["ROTATION"] = rotAccessorId;
-        }
-        if (hasScales) {
-          auto scaleAccessorId = createAccessorInGltf(
-              gltf,
-              instanceBufferViewId,
-              Accessor::ComponentType::FLOAT,
-              numInstances,
-              Accessor::Type::VEC3);
-          auto& scaleAccessor =
-              gltf.accessors[static_cast<uint32_t>(scaleAccessorId)];
-          scaleAccessor.byteOffset =
-              static_cast<int64_t>(dataBaseOffset + scaleOffset);
-          gpuExt.attributes["SCALE"] = scaleAccessorId;
-        }
+        auto rotAccessorId = createAccessorInGltf(
+            gltf,
+            instanceBufferViewId,
+            Accessor::ComponentType::FLOAT,
+            numInstances,
+            Accessor::Type::VEC4);
+        auto& rotAccessor =
+            gltf.accessors[static_cast<uint32_t>(rotAccessorId)];
+        rotAccessor.byteOffset =
+            static_cast<int64_t>(dataBaseOffset + rotOffset);
+        gpuExt.attributes["ROTATION"] = rotAccessorId;
+        auto scaleAccessorId = createAccessorInGltf(
+            gltf,
+            instanceBufferViewId,
+            Accessor::ComponentType::FLOAT,
+            numInstances,
+            Accessor::Type::VEC3);
+        auto& scaleAccessor =
+            gltf.accessors[static_cast<uint32_t>(scaleAccessorId)];
+        scaleAccessor.byteOffset =
+            static_cast<int64_t>(dataBaseOffset + scaleOffset);
+        gpuExt.attributes["SCALE"] = scaleAccessorId;
       });
   if (decodedInstances.rtcCenter) {
     applyRTC(*result.model, *decodedInstances.rtcCenter);
