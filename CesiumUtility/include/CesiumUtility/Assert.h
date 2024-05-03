85d70f10cf4294fdb91699f29eaa1bd2f9ad4292
Brian L <130494071+csciguy8@users.noreply.github.com>
2024-05-03 15:31:29 -0600

    Fix issue where reincluding cassert could break forced assertions

--- CesiumUtility/include/CesiumUtility/Assert.h
+++ CesiumUtility/include/CesiumUtility/Assert.h
@@ -1,21 +1,18 @@
 #pragma once
 
-// Allow assertion to be forcebly turned on, if we're configured
-#ifdef CESIUM_FORCE_ASSERTIONS
-// Asserts are normally compiled out when NDEBUG if defined. So undefine it
-// before including it
-#ifdef NDEBUG
-#undef NDEBUG
-#include <cassert>
-#define NDEBUG
+//
+// Define our own assertion, so that users can forcebly turn them on if needed
+//
+#if defined CESIUM_FORCE_ASSERTIONS && defined NDEBUG
+// Asserts are defined in cassert and normally compiled out when NDEBUG is
+// defined. Redirect to our own assertion that can't be compiled out
+namespace CesiumUtility {
+void forceAssertFailure();
+};
+#define ASSERT(expression)                                                     \
+  ((expression) ? 0 : CesiumUtility::forceAssertFailure())
 #else
+// Let assertions get defined with default behavior
 #include <cassert>
-#endif
-#else
-// Let assertions turn on with default behavior
-#include <cassert>
-#endif
-
-// Define our own assertion, so that users of this library can forcebly turn
-// them on if needed
 #define ASSERT(expression) assert(expression)
+#endif
