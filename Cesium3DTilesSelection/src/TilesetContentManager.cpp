ddeb117dafd09014d122521d434f9856a0fa23e5
Janine Liu <janine@cesium.com>
2024-06-18 13:54:21 -0400

    Formatting

--- Cesium3DTilesSelection/src/TilesetContentManager.cpp
+++ Cesium3DTilesSelection/src/TilesetContentManager.cpp
@@ -1455,8 +1455,8 @@ void TilesetContentManager::unloadDoneState(Tile& tile) {
   pRenderContent->setRenderResources(nullptr);
 }
 
-void TilesetContentManager::notifyTileStartLoading([
-    [maybe_unused]] const Tile* pTile) noexcept {
+void TilesetContentManager::notifyTileStartLoading(
+    [[maybe_unused]] const Tile* pTile) noexcept {
   ++this->_tileLoadsInProgress;
 }
 
