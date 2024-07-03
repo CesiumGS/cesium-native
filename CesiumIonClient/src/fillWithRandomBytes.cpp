e8b08710f13cfdbd038dabf5caf10b208d899436
Bradley Austin Davis <bdavis@saintandreas.org>
2024-07-02 18:01:13 -0700

    Just use SSL for random bytes

--- CesiumIonClient/src/fillWithRandomBytes.cpp
+++ CesiumIonClient/src/fillWithRandomBytes.cpp
@@ -1,64 +1,8 @@
-#define _CRT_RAND_S
 #include "fillWithRandomBytes.h"
 
-#include <cassert>
+#include <openssl/rand.h>
+
 #include <stdexcept>
-
-#if defined(_WIN32)
-#pragma comment(lib, "Bcrypt.lib")
-#include <Windows.h>
-#include <bcrypt.h>
-using ProviderHandle = BCRYPT_ALG_HANDLE;
-
-static ProviderHandle getProvider() {
-  static ProviderHandle ALG_HANDLE = nullptr;
-  if (ALG_HANDLE == nullptr) {
-    NTSTATUS status = BCryptOpenAlgorithmProvider(
-        &ALG_HANDLE,
-        BCRYPT_RNG_ALGORITHM,
-        nullptr,
-        0);
-    if (status != 0) {
-      throw std::runtime_error("Failed to open algorithm provider");
-    }
-  }
-  return ALG_HANDLE;
-}
-
-static void fillWithRandomBytesImpl(
-    ProviderHandle handle,
-    const gsl::span<uint8_t>& buffer) {
-  uint32_t cbBuffer = static_cast<uint32_t>(buffer.size());
-  NTSTATUS status = BCryptGenRandom(handle, buffer.data(), cbBuffer, 0);
-  if (status != 0) {
-    throw std::runtime_error("Failed to generate random bytes");
-  }
-}
-
-#else
-#include <cstdio>
-using ProviderHandle = FILE*;
-
-static ProviderHandle getProvider() {
-  static ProviderHandle HANDLE = nullptr;
-  if (HANDLE == nullptr) {
-    HANDLE = fopen("/dev/urandom", "rb");
-    assert(HANDLE != nullptr);
-  }
-  return HANDLE;
-}
-
-static void fillWithRandomBytesImpl(
-    ProviderHandle handle,
-    const gsl::span<uint8_t>& buffer) {
-  auto bufferSize = buffer.size();
-  auto readCount = fread((char*)buffer.data(), 1, bufferSize, handle);
-  if (readCount != bufferSize) {
-    throw std::runtime_error("Failed to generate random bytes");
-  }
-}
-
-#endif
 
 namespace CesiumIonClient {
 
@@ -66,8 +10,12 @@ void fillWithRandomBytes(const gsl::span<uint8_t>& buffer) {
   if (buffer.empty()) {
     return;
   }
-  ProviderHandle handle = getProvider();
-  ::fillWithRandomBytesImpl(handle, buffer);
+
+  int size = static_cast<int>(buffer.size());
+  auto result = RAND_bytes(buffer.data(), size);
+  if (result != 1) {
+    throw std::runtime_error("Failed to generate random bytes");
+  }
 }
 
 } // namespace CesiumIonClient
