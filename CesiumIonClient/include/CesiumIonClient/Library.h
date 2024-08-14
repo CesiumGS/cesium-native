f439583167f9d471b61d342f61390a7bc642e0d2
Cory Barr <cory.barr@gmail.com>
2024-08-14 12:46:30 -0700

    update CmakeLists and Library.h to generate updated HTML documentation

---
+++ CesiumIonClient/include/CesiumIonClient/Library.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * @brief Classes for working with Cesium ion clients
+ */
+namespace CesiumIonClient {}
+
+#if defined(_WIN32) && defined(CESIUM_SHARED)
+#ifdef CESIUMIONCLIENT_BUILDING
+#define CESIUMIONCLIENT_API __declspec(dllexport)
+#else
+#define CESIUMIONCLIENT_API __declspec(dllimport)
+#endif
+#else
+#define CESIUMIONCLIENT_API
+#endif
