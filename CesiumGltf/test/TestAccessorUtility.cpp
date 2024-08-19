daeca25354bf9e86e5aacfcedc2f7a0984e2d026
Janine Liu <janine@cesium.com>
2024-08-19 13:49:29 -0400

    Add more unit tests

--- CesiumGltf/test/TestAccessorUtility.cpp
+++ CesiumGltf/test/TestAccessorUtility.cpp
@@ -287,6 +287,93 @@ TEST_CASE("Test getFeatureIdAccessorView") {
   }
 }
 
+TEST_CASE("Test getFeatureIdAccessorView for instances") {
+  Model model;
+  std::vector<int8_t> featureIds{1, 2, 3, 4};
+
+  Buffer& buffer = model.buffers.emplace_back();
+  buffer.cesium.data.resize(featureIds.size() * sizeof(int8_t));
+  std::memcpy(
+      buffer.cesium.data.data(),
+      featureIds.data(),
+      buffer.cesium.data.size());
+  buffer.byteLength = static_cast<int64_t>(buffer.cesium.data.size());
+
+  BufferView& bufferView = model.bufferViews.emplace_back();
+  bufferView.buffer = 0;
+  bufferView.byteLength = buffer.byteLength;
+
+  Accessor& accessor = model.accessors.emplace_back();
+  accessor.bufferView = 0;
+  accessor.componentType = Accessor::ComponentType::BYTE;
+  accessor.type = Accessor::Type::SCALAR;
+  accessor.count = bufferView.byteLength;
+
+  Node& node = model.nodes.emplace_back();
+  ExtensionExtMeshGpuInstancing& instancingExtension =
+      node.addExtension<ExtensionExtMeshGpuInstancing>();
+  instancingExtension.attributes["_FEATURE_ID_0"] = 0;
+
+  SECTION("Handles missing extension") {
+    Node temporaryNode;
+
+    FeatureIdAccessorType featureIDAccessor =
+        getFeatureIdAccessorView(model, temporaryNode, 0);
+    REQUIRE(
+        std::visit(StatusFromAccessor{}, featureIDAccessor) !=
+        AccessorViewStatus::Valid);
+    REQUIRE(std::visit(CountFromAccessor{}, featureIDAccessor) == 0);
+
+    model.accessors[0].type = Accessor::Type::SCALAR;
+  }
+
+  SECTION("Handles invalid feature ID set index") {
+    FeatureIdAccessorType featureIDAccessor =
+        getFeatureIdAccessorView(model, node, 2);
+    REQUIRE(
+        std::visit(StatusFromAccessor{}, featureIDAccessor) !=
+        AccessorViewStatus::Valid);
+    REQUIRE(std::visit(CountFromAccessor{}, featureIDAccessor) == 0);
+  }
+
+  SECTION("Handles invalid accessor type") {
+    model.accessors[0].type = Accessor::Type::VEC2;
+
+    FeatureIdAccessorType featureIDAccessor =
+        getFeatureIdAccessorView(model, node, 0);
+    REQUIRE(
+        std::visit(StatusFromAccessor{}, featureIDAccessor) !=
+        AccessorViewStatus::Valid);
+    REQUIRE(std::visit(CountFromAccessor{}, featureIDAccessor) == 0);
+
+    model.accessors[0].type = Accessor::Type::SCALAR;
+  }
+
+  SECTION("Handles invalid normalized accessor") {
+    model.accessors[0].normalized = true;
+
+    FeatureIdAccessorType featureIDAccessor =
+        getFeatureIdAccessorView(model, node, 1);
+    REQUIRE(
+        std::visit(StatusFromAccessor{}, featureIDAccessor) !=
+        AccessorViewStatus::Valid);
+    REQUIRE(std::visit(CountFromAccessor{}, featureIDAccessor) == 0);
+
+    model.accessors[0].normalized = false;
+  }
+
+  SECTION("Retrieves from valid accessor") {
+    FeatureIdAccessorType featureIdAccessor =
+        getFeatureIdAccessorView(model, node, 0);
+    for (size_t i = 0; i < featureIds.size(); i++) {
+      int64_t featureID = std::visit(
+          FeatureIdFromAccessor{static_cast<int64_t>(i)},
+          featureIdAccessor);
+      REQUIRE(featureID == featureIds[i]);
+    }
+  }
+}
+
 TEST_CASE("FeatureIdFromAccessor") {
   Model model;
   std::vector<int8_t> featureIds{1, 2, 3, 4};
@@ -976,45 +1063,6 @@ TEST_CASE("Test TexCoordFromAccessor") {
       expected /= 255;
 
       REQUIRE(*maybeTexCoord == expected);
-    }
-  }
-}
-
-TEST_CASE("getFeatureIdAccessorView") {
-  Model model;
-  std::vector<int8_t> featureIds{1, 2, 3, 4};
-
-  Buffer& buffer = model.buffers.emplace_back();
-  buffer.cesium.data.resize(featureIds.size() * sizeof(int8_t));
-  std::memcpy(
-      buffer.cesium.data.data(),
-      featureIds.data(),
-      buffer.cesium.data.size());
-  buffer.byteLength = static_cast<int64_t>(buffer.cesium.data.size());
-
-  BufferView& bufferView = model.bufferViews.emplace_back();
-  bufferView.buffer = 0;
-  bufferView.byteLength = buffer.byteLength;
-
-  Accessor& accessor = model.accessors.emplace_back();
-  accessor.bufferView = 0;
-  accessor.componentType = Accessor::ComponentType::BYTE;
-  accessor.type = Accessor::Type::SCALAR;
-  accessor.count = bufferView.byteLength;
-
-  Node& node = model.nodes.emplace_back();
-  ExtensionExtMeshGpuInstancing& instancingExtension =
-      node.addExtension<ExtensionExtMeshGpuInstancing>();
-  instancingExtension.attributes["_FEATURE_ID_0"] = 0;
-
-  SECTION("Retrieves from valid accessor") {
-    FeatureIdAccessorType featureIdAccessor =
-        getFeatureIdAccessorView(model, node, 0);
-    for (size_t i = 0; i < featureIds.size(); i++) {
-      int64_t featureID = std::visit(
-          FeatureIdFromAccessor{static_cast<int64_t>(i)},
-          featureIdAccessor);
-      REQUIRE(featureID == featureIds[i]);
     }
   }
 }
