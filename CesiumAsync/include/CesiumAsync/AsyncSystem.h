a5829effbcf1f7172de1325af5b0aef5f8047643
Ashley Rogers <ashley.rogers@cesium.com>
2024-09-03 11:28:02 -0400

    Fix missing std::move

--- CesiumAsync/include/CesiumAsync/AsyncSystem.h
+++ CesiumAsync/include/CesiumAsync/AsyncSystem.h
@@ -315,7 +315,7 @@ private:
                   results.reserve(tasks.size());
 
                   for (auto it = tasks.begin(); it != tasks.end(); ++it) {
-                    results.emplace_back(it->get());
+                    results.emplace_back(std::move(it->get()));
                   }
                   return results;
                 });
