cedc1bbd05cd73d30518bd2e09efcebf22094670
Ashley Rogers <ashley.rogers@cesium.com>
2024-11-22 11:12:32 -0500

    Ubuntu 24.04 for clang-tidy CI, cast fix

--- CesiumGltf/include/CesiumGltf/NamedObject.h
+++ CesiumGltf/include/CesiumGltf/NamedObject.h
@@ -29,8 +29,8 @@ struct CESIUMGLTF_API NamedObject : public CesiumUtility::ExtensibleObject {
   int64_t getSizeBytes() const {
     int64_t accum = 0;
     accum += sizeof(NamedObject);
-    accum += ExtensibleObject::getSizeBytes() -
-             static_cast<int64_t>(sizeof(ExtensibleObject));
+    accum +=
+        ExtensibleObject::getSizeBytes() - int64_t(sizeof(ExtensibleObject));
     accum += this->name.capacity() * sizeof(char);
     return accum;
   }
