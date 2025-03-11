e4e563a58e6e7c9accf55b5a25f2d0c5fd119803
Kevin Ring <kevin@kotachrome.com>
2025-03-11 11:28:41 +1100

    More clang-tidy.

--- Cesium3DTilesSelection/src/TilesetHeightQuery.cpp
+++ Cesium3DTilesSelection/src/TilesetHeightQuery.cpp
@@ -18,6 +18,7 @@
 #include <CesiumGeospatial/GlobeRectangle.h>
 #include <CesiumGeospatial/S2CellBoundingVolume.h>
 #include <CesiumGltfContent/GltfUtilities.h>
+#include <CesiumUtility/IntrusivePointer.h>
 
 #include <glm/exponential.hpp>
 
