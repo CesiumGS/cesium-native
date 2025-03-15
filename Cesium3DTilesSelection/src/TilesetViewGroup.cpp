e8376528600206ebf6e1da743b02c924e6b43889
Kevin Ring <kevin@kotachrome.com>
2025-03-16 09:17:27 +1100

    Remove commented-out code.

--- Cesium3DTilesSelection/src/TilesetViewGroup.cpp
+++ Cesium3DTilesSelection/src/TilesetViewGroup.cpp
@@ -28,45 +28,6 @@ ViewUpdateResult& TilesetViewGroup::getViewUpdateResult() {
   return this->_updateResult;
 }
 
-// TileSelectionState
-// TilesetViewGroup::getPreviousSelectionState(const Tile& tile) const noexcept
-// {
-//   auto it = this->_previousSelectionStates.find(&tile);
-//   if (it == this->_previousSelectionStates.end()) {
-//     return TileSelectionState();
-//   } else {
-//     return it->second;
-//   }
-// }
-
-// TileSelectionState
-// TilesetViewGroup::getCurrentSelectionState(const Tile& tile) const noexcept {
-//   auto it = this->_currentSelectionStates.find(&tile);
-//   if (it == this->_currentSelectionStates.end()) {
-//     return TileSelectionState();
-//   } else {
-//     return it->second;
-//   }
-// }
-
-// void TilesetViewGroup::setCurrentSelectionState(
-//     const Tile& tile,
-//     const TileSelectionState& newState) noexcept {
-//   this->_currentSelectionStates[&tile] = newState;
-// }
-
-// void TilesetViewGroup::kick(const Tile& tile) noexcept {
-//   auto it = this->_currentSelectionStates.find(&tile);
-//   if (it == this->_currentSelectionStates.end()) {
-//     // There should already be a selection result for this tile prior to
-//     // kicking.
-//     CESIUM_ASSERT(false);
-//     return;
-//   } else {
-//     it->second.kick();
-//   }
-// }
-
 void TilesetViewGroup::addToLoadQueue(const TileLoadTask& task) {
   Tile* pTile = task.pTile;
   CESIUM_ASSERT(pTile != nullptr);
