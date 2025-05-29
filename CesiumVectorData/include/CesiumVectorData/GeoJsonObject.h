ebff6c79c2c9a5a5d02806880bef66526344fa6d
Ashley Rogers <Ashley.Rogers@Bentley.com>
2025-05-29 11:25:56 -0400

    Documentation

--- CesiumVectorData/include/CesiumVectorData/GeoJsonObject.h
+++ CesiumVectorData/include/CesiumVectorData/GeoJsonObject.h
@@ -2,6 +2,8 @@
 
 #include "GeoJsonObjectTypes.h"
 
+#include <CesiumGeometry/AxisAlignedBox.h>
+
 #include <stack>
 #include <vector>
 
@@ -13,12 +15,24 @@ template <typename SingleT, typename MultiT, typename ValueT>
 struct ConstGeoJsonPrimitiveIterator;
 template <typename ObjectType> struct ConstGeoJsonObjectTypeIterator;
 
+/**
+ * @brief An iterator over all `Point` and `MultiPoint` objects in and including
+ * a root GeoJSON object.
+ */
 using ConstGeoJsonPointIterator =
     ConstGeoJsonPrimitiveIterator<GeoJsonPoint, GeoJsonMultiPoint, glm::dvec3>;
+/**
+ * @brief An iterator over all `LineString` and `MultiLineString` objects in and
+ * including a root GeoJSON object.
+ */
 using ConstGeoJsonLineStringIterator = ConstGeoJsonPrimitiveIterator<
     GeoJsonLineString,
     GeoJsonMultiLineString,
     std::vector<glm::dvec3>>;
+/**
+ * @brief An iterator over all `Polygon` and `MultiPolygon` objects in and
+ * including a root GeoJSON object.
+ */
 using ConstGeoJsonPolygonIterator = ConstGeoJsonPrimitiveIterator<
     GeoJsonPolygon,
     GeoJsonMultiPolygon,
@@ -28,39 +42,103 @@ using ConstGeoJsonPolygonIterator = ConstGeoJsonPrimitiveIterator<
  * @brief A wrapper around an object in a GeoJSON document.
  */
 struct GeoJsonObject {
+  /**
+   * @brief An object providing `begin` and `end` methods for creating iterators
+   * of the given type for a \ref GeoJsonObject.
+   */
   template <typename IteratorType> struct IteratorProvider {
-    IteratorProvider(const GeoJsonObject* pObject) : _pObject(pObject) {}
-
+    /**
+     * @brief Returns an iterator pointing to the first element.
+     */
     IteratorType begin() { return IteratorType(*_pObject); }
 
+    /**
+     * @brief Returns an iterator pointing "past the end" of all the elements.
+     */
     IteratorType end() { return IteratorType(); }
 
   private:
+    IteratorProvider(const GeoJsonObject* pObject) : _pObject(pObject) {}
+
     const GeoJsonObject* _pObject;
+    friend struct GeoJsonObject;
   };
 
+  /**
+   * @brief Returns an iterator pointing to this object. Iterating this will
+   * provide all children of this object.
+   */
   GeoJsonObjectIterator begin();
+  /**
+   * @brief Returns an iterator pointing "past the end" of the list of children
+   * of this object.
+   */
   GeoJsonObjectIterator end();
+
+  /**
+   * @brief Returns an iterator pointing to this object. Iterating this will
+   * provide all children of this object.
+   */
   ConstGeoJsonObjectIterator begin() const;
+  /**
+   * @brief Returns an iterator pointing "past the end" of the list of children
+   * of this object.
+   */
   ConstGeoJsonObjectIterator end() const;
 
-  IteratorProvider<ConstGeoJsonPointIterator> points() const {
-    return IteratorProvider<ConstGeoJsonPointIterator>(this);
-  }
+  /**
+   * @brief Allows iterating over all points defined in this object or any child
+   * objects. This will include both `Point` objects and `MultiPoint` objects.
+   */
+  IteratorProvider<ConstGeoJsonPointIterator> points() const;
 
-  IteratorProvider<ConstGeoJsonLineStringIterator> lines() const {
-    return IteratorProvider<ConstGeoJsonLineStringIterator>(this);
-  }
+  /**
+   * @brief Allows iterating over all lines defined in this object or any child
+   * objects. This will include both `LineString` objects and `MultiLineString`
+   * objects.
+   */
+  IteratorProvider<ConstGeoJsonLineStringIterator> lines() const;
 
-  IteratorProvider<ConstGeoJsonPolygonIterator> polygons() const {
-    return IteratorProvider<ConstGeoJsonPolygonIterator>(this);
-  }
+  /**
+   * @brief Allows iterating over all polygons defined in this object or any
+   * child objects. This will include both `Polygon` objects and `MultiPolygon`
+   * objects.
+   */
+  IteratorProvider<ConstGeoJsonPolygonIterator> polygons() const;
 
+  /**
+   * @brief Returns all \ref GeoJsonObject values matching the given type in
+   * this object or in any children.
+   */
   template <typename ObjectType>
   IteratorProvider<ConstGeoJsonObjectTypeIterator<ObjectType>>
   allOfType() const {
     return IteratorProvider<ConstGeoJsonObjectTypeIterator<ObjectType>>(this);
   }
+
+  /**
+   * @brief Returns the bounding box defined in the GeoJSON for this object, if
+   * any.
+   */
+  const std::optional<CesiumGeometry::AxisAlignedBox>& getBoundingBox() const;
+
+  /**
+   * @brief Returns the bounding box defined in the GeoJSON for this object, if
+   * any.
+   */
+  std::optional<CesiumGeometry::AxisAlignedBox>& getBoundingBox();
+
+  /**
+   * @brief Returns the \ref CesiumUtility::JsonValue::Object containing any
+   * foreign members on this GeoJSON object.
+   */
+  const CesiumUtility::JsonValue::Object& getForeignMembers() const;
+
+  /**
+   * @brief Returns the \ref CesiumUtility::JsonValue::Object containing any
+   * foreign members on this GeoJSON object.
+   */
+  CesiumUtility::JsonValue::Object& getForeignMembers();
 
   /**
    * @brief Returns the \ref GeoJsonObjectType that this \ref GeoJsonObject
@@ -131,7 +209,9 @@ struct GeoJsonObjectIterator {
   using pointer = GeoJsonObject*;
   using reference = GeoJsonObject&;
 
+  /** @brief Returns a reference to the current object. */
   reference operator*() const { return *_pCurrentObject; }
+  /** @brief Returns a pointer to the current object. */
   pointer operator->() { return _pCurrentObject; }
 
   /**
@@ -151,20 +231,36 @@ struct GeoJsonObjectIterator {
     return nullptr;
   }
 
+  /**
+   * @brief Returns `true` if this is an "end" iterator (points past the end of
+   * all objects).
+   */
   bool isEnded() const {
     return this->_nextPosStack.empty() && this->_objectStack.empty() &&
            this->_pCurrentObject == nullptr;
   }
 
+  /**
+   * @brief Iterates to the next \ref GeoJsonObject, returning this modified
+   * iterator.
+   */
   GeoJsonObjectIterator& operator++() {
     this->iterate();
     return *this;
   }
+  /**
+   * @brief Iterates to the next \ref GeoJsonObject, returning the previous
+   * state of the iterator.
+   */
   GeoJsonObjectIterator operator++(int) {
     GeoJsonObjectIterator tmp = *this;
     this->iterate();
     return tmp;
   }
+
+  /**
+   * @brief Checks if two \ref GeoJsonObjectIterator iterators are equal.
+   */
   friend bool
   operator==(const GeoJsonObjectIterator& a, const GeoJsonObjectIterator& b) {
     if (a._pCurrentObject != b._pCurrentObject) {
@@ -186,11 +282,22 @@ struct GeoJsonObjectIterator {
     return a._objectStack.empty() == b._objectStack.empty() &&
            a._nextPosStack.empty() == b._nextPosStack.empty();
   }
+
+  /**
+   * @brief Checks if two \ref GeoJsonObjectIterator iterators are not equal.
+   */
   friend bool
   operator!=(const GeoJsonObjectIterator& a, const GeoJsonObjectIterator& b) {
     return !(a == b);
   }
 
+  /**
+   * @brief Creates a new \ref GeoJsonObjectIterator with the given \ref
+   * GeoJsonObject as the root object.
+   *
+   * @param rootObject The root object of the new iterator. This will be the
+   * first object returned.
+   */
   GeoJsonObjectIterator(GeoJsonObject& rootObject)
       : _objectStack(), _nextPosStack(), _pCurrentObject(nullptr) {
     this->_objectStack.emplace_back(&rootObject);
@@ -198,6 +305,10 @@ struct GeoJsonObjectIterator {
     this->iterate();
   }
 
+  /**
+   * @brief Creates a new \ref GeoJsonObjectIterator without any \ref
+   * GeoJsonObject. This is equivalent to an "end" iterator.
+   */
   GeoJsonObjectIterator()
       : _objectStack(), _nextPosStack(), _pCurrentObject(nullptr) {}
 
@@ -281,6 +392,9 @@ private:
   friend struct ConstGeoJsonObjectIterator;
 };
 
+/**
+ * @brief The `const` equivalent of \ref GeoJsonObjectIterator.
+ */
 struct ConstGeoJsonObjectIterator {
   using iterator_category = std::forward_iterator_tag;
   using difference_type = std::ptrdiff_t;
@@ -288,39 +402,84 @@ struct ConstGeoJsonObjectIterator {
   using pointer = const GeoJsonObject*;
   using reference = const GeoJsonObject&;
 
+  /** @brief Returns a reference to the current object. */
   reference operator*() const { return *this->_it; }
+  /** @brief Returns a pointer to the current object. */
   pointer operator->() { return this->_it._pCurrentObject; }
 
+  /**
+   * @brief Attempts to find the Feature that contains the current item the
+   * iterator is pointing to.
+   *
+   * If the iterator is pointing to a Feature, that Feature will be returned.
+   */
   const GeoJsonObject* getFeature() { return this->_it.getFeature(); }
 
+  /**
+   * @brief Iterates to the next \ref GeoJsonObject, returning this modified
+   * iterator.
+   */
   ConstGeoJsonObjectIterator& operator++() {
     ++this->_it;
     return *this;
   }
+  /**
+   * @brief Iterates to the next \ref GeoJsonObject, returning the previous
+   * state of the iterator.
+   */
   ConstGeoJsonObjectIterator operator++(int) {
     ConstGeoJsonObjectIterator tmp = *this;
     this->_it++;
     return tmp;
   }
+  /**
+   * @brief Checks if two \ref ConstGeoJsonObjectIterator iterators are equal.
+   */
   friend bool operator==(
       const ConstGeoJsonObjectIterator& a,
       const ConstGeoJsonObjectIterator& b) {
     return a._it == b._it;
   }
+
+  /**
+   * @brief Checks if two \ref ConstGeoJsonObjectIterator iterators are not
+   * equal.
+   */
   friend bool operator!=(
       const ConstGeoJsonObjectIterator& a,
       const ConstGeoJsonObjectIterator& b) {
     return a._it != b._it;
   }
 
+  /**
+   * @brief Creates a new \ref ConstGeoJsonObjectIterator with the given \ref
+   * GeoJsonObject as the root object.
+   *
+   * @param rootObject The root object of the new iterator. This will be the
+   * first object returned.
+   */
   ConstGeoJsonObjectIterator(const GeoJsonObject& rootObject)
       : _it(const_cast<GeoJsonObject&>(rootObject)) {}
+  /**
+   * @brief Creates a new \ref ConstGeoJsonObjectIterator without any \ref
+   * GeoJsonObject. This is equivalent to an "end" iterator.
+   */
   ConstGeoJsonObjectIterator() = default;
 
 private:
   GeoJsonObjectIterator _it;
 };
 
+/**
+ * @brief Returns all geometry data of a given type from a \ref GeoJsonObject.
+ *
+ * @tparam SingleT The type of the "single" version of this geometry object. For
+ * example, `Point`.
+ * @tparam MultiT The type of the "multi" version of this geometry object. For
+ * example, `MultiPoint`.
+ * @tparam ValueT The type of the geometry data included in both
+ * `SingleT::coordinates` and `MultiT::coordinates[i]`.
+ */
 template <typename SingleT, typename MultiT, typename ValueT>
 struct ConstGeoJsonPrimitiveIterator {
   using iterator_category = std::forward_iterator_tag;
@@ -329,37 +488,61 @@ struct ConstGeoJsonPrimitiveIterator {
   using pointer = const ValueT*;
   using reference = const ValueT&;
 
+  /**
+   * @brief Returns a reference to the current value.
+   */
   reference operator*() const {
-    const GeoJsonMultiPoint* pMultiPoint =
-        (*this->_it).getIf<GeoJsonMultiPoint>();
+    const GeoJsonMultiPoint* pMultiPoint = (*this->_it).getIf<MultiT>();
     if (pMultiPoint) {
       return pMultiPoint->coordinates[this->_currentMultiIdx];
     }
 
-    return (*this->_it).get<GeoJsonPoint>().coordinates;
+    return (*this->_it).get<SingleT>().coordinates;
   }
+  /**
+   * @brief Returns a pointer to the current value.
+   */
   pointer operator->() { return &**this; }
 
+  /**
+   * @brief Iterates to the next value, returning the modified iterator.
+   */
   ConstGeoJsonPrimitiveIterator& operator++() {
     this->iterate();
     return *this;
   }
+  /**
+   * @brief Iterates to the next value, returning the previous state of the
+   * iterator.
+   */
   ConstGeoJsonPrimitiveIterator operator++(int) {
     ConstGeoJsonPrimitiveIterator tmp = *this;
     this->_it++;
     return tmp;
   }
+  /**
+   * @brief Checks if two \ref ConstGeoJsonPrimitiveIterator iterators are
+   * equal.
+   */
   friend bool operator==(
       const ConstGeoJsonPrimitiveIterator& a,
       const ConstGeoJsonPrimitiveIterator& b) {
     return a._it == b._it;
   }
+  /**
+   * @brief Checks if two \ref ConstGeoJsonPrimitiveIterator iterators are not
+   * equal.
+   */
   friend bool operator!=(
       const ConstGeoJsonPrimitiveIterator& a,
       const ConstGeoJsonPrimitiveIterator& b) {
     return a._it != b._it;
   }
 
+  /**
+   * @brief Creates a new \ref ConstGeoJsonPrimitiveIterator from the given root
+   * \ref GeoJsonObject.
+   */
   ConstGeoJsonPrimitiveIterator(const GeoJsonObject& rootObject)
       : _it(const_cast<GeoJsonObject&>(rootObject)) {
     if (!_it.isEnded() &&
@@ -367,6 +550,10 @@ struct ConstGeoJsonPrimitiveIterator {
       this->iterate();
     }
   }
+
+  /**
+   * @brief Creates an empty \ref ConstGeoJsonPrimitiveIterator.
+   */
   ConstGeoJsonPrimitiveIterator() = default;
 
 private:
@@ -393,6 +580,10 @@ private:
   size_t _currentMultiIdx = 0;
 };
 
+/**
+ * @brief An iterator over all \ref GeoJsonObject objects that contain a value
+ * of type `ObjectType`.
+ */
 template <typename ObjectType> struct ConstGeoJsonObjectTypeIterator {
   using iterator_category = std::forward_iterator_tag;
   using difference_type = std::ptrdiff_t;
@@ -400,35 +591,64 @@ template <typename ObjectType> struct ConstGeoJsonObjectTypeIterator {
   using pointer = const ObjectType*;
   using reference = const ObjectType&;
 
+  /** @brief Returns a reference to the current object. */
   reference operator*() const {
     return (*this->_it).get<ObjectType>().coordinates;
   }
+  /** @brief Returns a pointer to the current object. */
   pointer operator->() { return &**this; }
 
+  /**
+   * @brief Iterates to the next \ref ObjectType, returning this modified
+   * iterator.
+   */
   ConstGeoJsonObjectTypeIterator& operator++() {
     do {
       ++this->_it;
     } while (!this->_it.isEnded() && this->_it->containsType<ObjectType>());
     return *this;
   }
+  /**
+   * @brief Iterates to the next \ref ObjectType, returning the previous
+   * state of the iterator.
+   */
   ConstGeoJsonObjectTypeIterator operator++(int) {
     ConstGeoJsonObjectTypeIterator tmp = *this;
     this->_it++;
     return tmp;
   }
+  /**
+   * @brief Checks if two \ref ConstGeoJsonObjectTypeIterator iterators are
+   * equal.
+   */
   friend bool operator==(
       const ConstGeoJsonObjectTypeIterator& a,
       const ConstGeoJsonObjectTypeIterator& b) {
     return a._it == b._it;
   }
+  /**
+   * @brief Checks if two \ref ConstGeoJsonObjectTypeIterator iterators are not
+   * equal.
+   */
   friend bool operator!=(
       const ConstGeoJsonObjectTypeIterator& a,
       const ConstGeoJsonObjectTypeIterator& b) {
     return a._it != b._it;
   }
 
+  /**
+   * @brief Creates a new \ref ConstGeoJsonObjectTypeIterator with the given
+   * \ref GeoJsonObject as the root object.
+   *
+   * @param rootObject The root object of the new iterator. This will be the
+   * first object returned.
+   */
   ConstGeoJsonObjectTypeIterator(const GeoJsonObject& rootObject)
       : _it(const_cast<GeoJsonObject&>(rootObject)) {}
+  /**
+   * @brief Creates a new \ref ConstGeoJsonObjectTypeIterator without any \ref
+   * GeoJsonObject. This is equivalent to an "end" iterator.
+   */
   ConstGeoJsonObjectTypeIterator() = default;
 
 private:
