4da88c7cf42f103cb649008f463a16076e77d556
Ashley Rogers <Ashley.Rogers@Bentley.com>
2025-07-02 15:59:46 -0400

    All working!*

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -5,8 +5,8 @@
 # endif()
 
 if(CESIUM_TARGET_WASM)
-  add_compile_options(-sMEMORY64=1 -pthread -msimd128 -mnontrapping-fptoint -sSUPPORT_LONGJMP=emscripten)
-  add_link_options(-sMEMORY64=1 -pthread -sALLOW_MEMORY_GROWTH=1 -sPTHREAD_POOL_SIZE=4 -sMAXIMUM_MEMORY=8589934592 -sMIN_NODE_VERSION=200000 -sINITIAL_MEMORY=268435456 -sSTACK_SIZE=1048576 -sDISABLE_EXCEPTION_CATCHING=0 -mbulk-memory -mnontrapping-fptoint -msse4.2 -sMALLOC=mimalloc -sWASM_BIGINT -sSUPPORT_LONGJMP=emscripten -sFORCE_FILESYSTEM -sPROXY_TO_PTHREAD)
+  add_compile_options(-sMEMORY64=1 -pthread -msimd128 -mnontrapping-fptoint -fwasm-exceptions -sSUPPORT_LONGJMP=wasm)
+  add_link_options(-sMEMORY64=1 -pthread -sALLOW_MEMORY_GROWTH=1 -sPTHREAD_POOL_SIZE=4 -sMAXIMUM_MEMORY=8589934592 -sMIN_NODE_VERSION=200000 -sINITIAL_MEMORY=268435456 -sSTACK_SIZE=1048576 -fwasm-exceptions -mbulk-memory -mnontrapping-fptoint -msse4.2 -sMALLOC=mimalloc -sWASM_BIGINT -sSUPPORT_LONGJMP=wasm -sFORCE_FILESYSTEM -sPROXY_TO_PTHREAD)
 
   add_link_options($<$<CONFIG:Debug>:-gsource-map>)
 endif()
