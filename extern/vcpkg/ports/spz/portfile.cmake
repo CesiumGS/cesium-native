6efafe6bc9a57a657256ba0774567848ffd8a943
Ashley Rogers <ashley.rogers@bentley.com>
2025-08-20 13:44:35 -0400

    Just use non-ng zlib, seems to have fewer issues

--- extern/vcpkg/ports/spz/portfile.cmake
+++ extern/vcpkg/ports/spz/portfile.cmake
@@ -1,12 +1,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nianticlabs/spz
-    REF "v2.0.0"
-    SHA512 e1ee9314bd0a698e73db6d02937a20eea9419864d45eaa03e184cd9fca07dc08a92c2dfedf8e2415cbfc1ca3917435eb54ad0badd0f584aeeb2038b3bf7f000a
+    REF "8767f22a39c32f8190b2e0f9ba256516c74e7b73"
+    SHA512 fb0154b8154e252d08b9751df3b66e75baf21cd4c9cec9c8de520f7cee62e86ab1d27f4d5a64f44e8bdac7b72fe465860181aa5b726c252742c6611ed91d9437
     HEAD_REF main
     PATCHES
       loadSpz.patch
-      useZlibng.patch
 )
 
 vcpkg_cmake_configure(
