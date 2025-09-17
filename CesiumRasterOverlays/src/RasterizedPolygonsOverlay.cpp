2044ee4508ecd5cedc564f3f0c46f39c0b54bb2f
Ashley Rogers <ashley.rogers@bentley.com>
2025-09-17 14:47:39 -0400

    Revert polygon overlay implementation changes

--- CesiumRasterOverlays/src/RasterizedPolygonsOverlay.cpp
+++ CesiumRasterOverlays/src/RasterizedPolygonsOverlay.cpp
@@ -36,22 +36,21 @@ namespace {
 void rasterizePolygons(
     LoadedRasterOverlayImage& loaded,
     const CesiumGeospatial::GlobeRectangle& rectangle,
-    const CesiumGeospatial::Ellipsoid& ellipsoid,
     const glm::dvec2& textureSize,
     const std::vector<CartographicPolygon>& cartographicPolygons,
     bool invertSelection) {
 
   CesiumGltf::ImageAsset& image = loaded.pImage.emplace();
 
-  uint8_t insideColor;
-  uint8_t outsideColor;
+  std::byte insideColor;
+  std::byte outsideColor;
 
   if (invertSelection) {
-    insideColor = 0;
-    outsideColor = 0xff;
+    insideColor = static_cast<std::byte>(0);
+    outsideColor = static_cast<std::byte>(0xff);
   } else {
-    insideColor = 0xff;
-    outsideColor = 0;
+    insideColor = static_cast<std::byte>(0xff);
+    outsideColor = static_cast<std::byte>(0);
   }
 
   // create a 1x1 mask if the rectangle is completely inside a polygon
@@ -63,7 +62,7 @@ void rasterizePolygons(
     image.height = 1;
     image.channels = 1;
     image.bytesPerChannel = 1;
-    image.pixelData.resize(1, std::byte{insideColor});
+    image.pixelData.resize(1, insideColor);
     return;
   }
 
@@ -86,38 +85,88 @@ void rasterizePolygons(
     image.height = 1;
     image.channels = 1;
     image.bytesPerChannel = 1;
-    image.pixelData.resize(1, std::byte{outsideColor});
+    image.pixelData.resize(1, outsideColor);
     return;
   }
+
+  const double rectangleWidth = rectangle.computeWidth();
+  const double rectangleHeight = rectangle.computeHeight();
 
   // create source image
   loaded.moreDetailAvailable = true;
   image.width = int32_t(glm::round(textureSize.x));
   image.height = int32_t(glm::round(textureSize.y));
-  image.channels = 4;
+  image.channels = 1;
   image.bytesPerChannel = 1;
-  image.pixelData.resize(
-      size_t(image.width * image.height * 4),
-      std::byte{outsideColor});
+  image.pixelData.resize(size_t(image.width * image.height), outsideColor);
 
-  CesiumVectorData::VectorRasterizer rasterizer(
-      rectangle,
-      loaded.pImage,
-      0,
-      ellipsoid);
-  rasterizer.clear(CesiumUtility::Color{outsideColor, 0x00, 0x00, 0xff});
+  // TODO: this is naive approach, use line-triangle
+  // intersections to rasterize one row at a time
+  // NOTE: also completely ignores antimeridian (really these
+  // calculations should be normalized to the first vertex)
+  for (const CartographicPolygon& polygon : cartographicPolygons) {
+    const std::vector<glm::dvec2>& vertices = polygon.getVertices();
+    const std::vector<uint32_t>& indices = polygon.getIndices();
+    for (size_t triangle = 0; triangle < indices.size() / 3; ++triangle) {
+      const glm::dvec2& a = vertices[indices[3 * triangle]];
+      const glm::dvec2& b = vertices[indices[3 * triangle + 1]];
+      const glm::dvec2& c = vertices[indices[3 * triangle + 2]];
 
-  CesiumVectorData::PolygonStyle insideStyle{
-      CesiumVectorData::ColorStyle{
-          CesiumUtility::Color{insideColor, 0x00, 0x00, 0xff}},
-      std::nullopt};
-  for (const CartographicPolygon& selection : cartographicPolygons) {
-    rasterizer.drawPolygon(selection, insideStyle);
+      // TODO: deal with the corner cases here
+      const double minX = glm::min(a.x, glm::min(b.x, c.x));
+      const double minY = glm::min(a.y, glm::min(b.y, c.y));
+      const double maxX = glm::max(a.x, glm::max(b.x, c.x));
+      const double maxY = glm::max(a.y, glm::max(b.y, c.y));
+
+      const CesiumGeospatial::GlobeRectangle triangleBounds(
+          minX, 
+          minY,
+          maxX,
+          maxY);
+
+      // skip this triangle if it is entirely outside the tile bounds
+      if (!rectangle.computeIntersection(triangleBounds)) {
+        continue;
+      }
+
+      const glm::dvec2 ab = b - a;
+      const glm::dvec2 ab_perp(-ab.y, ab.x);
+      const glm::dvec2 bc = c - b;
+      const glm::dvec2 bc_perp(-bc.y, bc.x);
+      const glm::dvec2 ca = a - c;
+      const glm::dvec2 ca_perp(-ca.y, ca.x);
+
+      size_t width = size_t(image.width);
+      size_t height = size_t(image.height);
+
+      for (size_t j = 0; j < height; ++j) {
+        const double pixelY =
+            rectangle.getSouth() +
+            rectangleHeight * (1.0 - (double(j) + 0.5) / double(height));
+        for (size_t i = 0; i < width; ++i) {
+          const double pixelX = rectangle.getWest() + rectangleWidth *
+                                                          (double(i) + 0.5) /
+                                                          double(width);
+          const glm::dvec2 v(pixelX, pixelY);
+
+          const glm::dvec2 av = v - a;
+          const glm::dvec2 cv = v - c;
+
+          const double v_proj_ab_perp = glm::dot(av, ab_perp);
+          const double v_proj_bc_perp = glm::dot(cv, bc_perp);
+          const double v_proj_ca_perp = glm::dot(cv, ca_perp);
+
+          // will determine in or out, irrespective of winding
+          if ((v_proj_ab_perp >= 0.0 && v_proj_ca_perp >= 0.0 &&
+               v_proj_bc_perp >= 0.0) ||
+              (v_proj_ab_perp <= 0.0 && v_proj_ca_perp <= 0.0 &&
+               v_proj_bc_perp <= 0.0)) {
+            image.pixelData[width * j + i] = insideColor;
+          }
+        }
+      }
+    }
   }
-
-  rasterizer.finalize();
-  // Convert RGBA32 -> R8
-  image.changeNumberOfChannels(1);
 }
 } // namespace
 
@@ -179,7 +228,6 @@ public:
           rasterizePolygons(
               result,
               tileRectangle,
-              getProjectionEllipsoid(projection),
               textureSize,
               polygons,
               invertSelection);
