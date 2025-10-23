33d5640416ba3a4be6eb69ecf5047d94681aa2cd
Kevin Ring <kevin@kotachrome.com>
2025-10-23 20:47:46 +1100

    Update spz overlay port.
    It now matches the official vcpkg one as of `2025.09.17`, plus it adds
    the `loadSpz.patch` patch to add an overload of loadSpz taking a raw
    pointer.

--- extern/vcpkg/ports/spz/portfile.cmake
+++ extern/vcpkg/ports/spz/portfile.cmake
@@ -1,20 +1,26 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nianticlabs/spz
-    REF "8767f22a39c32f8190b2e0f9ba256516c74e7b73"
-    SHA512 fb0154b8154e252d08b9751df3b66e75baf21cd4c9cec9c8de520f7cee62e86ab1d27f4d5a64f44e8bdac7b72fe465860181aa5b726c252742c6611ed91d9437
+    REF v${VERSION}
+    SHA512 e1ee9314bd0a698e73db6d02937a20eea9419864d45eaa03e184cd9fca07dc08a92c2dfedf8e2415cbfc1ca3917435eb54ad0badd0f584aeeb2038b3bf7f000a
     HEAD_REF main
     PATCHES
-      loadSpz.patch
+        android-log.diff
+        loadSpz.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DBUILD_SHARED_LIBS=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/spz")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
