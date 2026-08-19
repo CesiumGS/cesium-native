ba21be2521961f8c85e48f7be3c2e88501a03232
Tim Moore <tim.moore@cesium.com>
2026-08-19 13:57:23 +0200

    Fix documentation

--- Cesium3DTilesSelection/include/Cesium3DTilesSelection/GeneralCullingVolume.h
+++ Cesium3DTilesSelection/include/Cesium3DTilesSelection/GeneralCullingVolume.h
@@ -8,9 +8,24 @@
 
 namespace Cesium3DTilesSelection {
 
+/**
+ * @brief A generalization of @ref CesiumGeometry::CullingVolume.
+ *
+ * CesiumGeometry::CullingVolume represents orthographic and perspective view
+ * frustums; GeneralCullingVolume includes @ref
+ * Cesium3DTilesSelection::BoundingVolume too, allowing filtering on
+ * e.g. geographic areas.
+ */
 using GeneralCullingVolume =
     std::variant<CesiumGeometry::CullingVolume, BoundingVolume>;
 
+/**
+ * @brief test if a general culling volume intersects a bounding volume.
+ *
+ * @param CullingVolume the general culling volume.
+ * @param BoundingVolume the bounding volume.
+ * @return true if volumes intersect.
+ */
 bool CESIUM3DTILESSELECTION_API isBoundingVolumeVisible(
     const GeneralCullingVolume& cullingVolume,
     const BoundingVolume& boundingVolume);
